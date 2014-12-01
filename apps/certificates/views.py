23a8b06b3708b40179db900f236093cd1c36ad5a
Nate Otto <nate@nate-4.local>
2014-12-01 11:29:33 -0800

    pep8 cleanup

--- apps/certificates/views.py
+++ apps/certificates/views.py
@@ -3,37 +3,32 @@ from certificates.models import *
 from mainsite.views import ActiveTabMixin
 from certificates.forms import CertificateForm
 
-import json
 
 class CertificateCreate(ActiveTabMixin, CreateView):
     model = Certificate
     active_tab = 'certificates'
     form_class = CertificateForm
-    
+
 
 class CertificateDetail(ActiveTabMixin, DetailView):
     model = Certificate
     active_tab = 'certificates'
-    
 
     def get_context_data(self, **kwargs):
-    	context = super(CertificateDetail, self).get_context_data(**kwargs)
+        context = super(CertificateDetail, self).get_context_data(**kwargs)
 
-    	certificate = context['object']
-    	badge = certificate.open_badge
-    	context['badge_name'] = badge.getProp('badgeclass', 'name')
-    	context['badge_description'] = badge.getProp('badgeclass', 'description')
-    	context['badge_image'] = badge.getProp('badgeclass', 'image')
-    	context['issue_date'] = badge.getProp('assertion', 'issuedOn')
-    	context['badge_issuer'] = badge.getProp('issuerorg', 'name')
-    	context['issuer_url'] = badge.getProp('issuerorg', 'url')
+        certificate = context['object']
+        badge = certificate.open_badge
+        context['badge_name'] = badge.getProp('badgeclass', 'name')
+        context['badge_description'] = badge.getProp('badgeclass', 'description')
+        context['badge_image'] = badge.getProp('badgeclass', 'image')
+        context['issue_date'] = badge.getProp('assertion', 'issuedOn')
+        context['badge_issuer'] = badge.getProp('issuerorg', 'name')
+        context['issuer_url'] = badge.getProp('issuerorg', 'url')
 
-    	context['recipient'] = badge.recipient_input
+        context['recipient'] = badge.recipient_input
 
-    	context['baked_image_url'] = badge.getProp('assertion', 'image')
-    	if context['baked_image_url']:
-    		context['qrcode'] = "http://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=" + context['baked_image_url']
-    	import pdb; pdb.set_trace();
-    	return context
-
-
+        context['baked_image_url'] = badge.getProp('assertion', 'image')
+        if context['baked_image_url']:
+            context['qrcode'] = "http://chart.googleapis.com/chart?cht=qr&chs=400x400&chl=" + context['baked_image_url']
+        return context
