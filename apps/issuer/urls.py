089580fe1c73c8c3bdaa01144c458b6e4c792fde
Nate Otto <notto@concentricsky.com>
2015-02-27 11:25:25 -0800

    Update all issuer URLs to deliver basic template and issuer context. (still needs refinement)

--- apps/issuer/urls.py
+++ apps/issuer/urls.py
@@ -3,7 +3,5 @@ from django.contrib.auth.decorators import login_required
 from issuer.views import *
 
 urlpatterns = patterns('issuer.views',
-    url(r'/create$', login_required(IssuerCreate.as_view()), name='issuer_create'),
-    url(r'/(?P<pk>\d+)', IssuerDetail.as_view(), name='issuer_detail'),
-    url(r'/notifications$', EarnerNotificationCreate.as_view(), name='notify_earner')
+    url(r'^', EarnerNotificationCreate.as_view(), name='notify_earner')
 )
