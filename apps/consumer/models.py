1f4571a1c008f5114e508d088ebd0d02b9b406c0
Nate Otto <notto@concentricsky.com>
2015-03-03 16:50:12 -0800

    Created ConsumerBadge model and accompanying migrations.

---
+++ apps/consumer/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+import cachemodel
+
+from badgeanalysis.models import OpenBadge
+
+
+class ConsumerBadge(cachemodel.CacheModel):
+    consumer = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
+    badge = models.OneToOneField(OpenBadge, related_name='consumerbadge', null=True, blank=False)
+
+    def __unicode__(self):
+        return str(self.badge)
