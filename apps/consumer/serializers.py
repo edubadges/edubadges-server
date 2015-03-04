1288c00b27ac952fab9a554df89f6808414898f6
Nate Otto <notto@concentricsky.com>
2015-03-04 13:29:03 -0800

    Add ConsumerBadges to BadgeUserSerializer to include them in iniatialData in views.

--- apps/consumer/serializers.py
+++ apps/consumer/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from rest_framework.exceptions import ValidationError
-from models import ConsumerBadge
 from badgeanalysis.validation_messages import BadgeValidationError
 from badgeanalysis.models import OpenBadge
 from badgeanalysis.serializers import BadgeSerializer
@@ -9,5 +8,4 @@ from badgeanalysis.serializers import BadgeSerializer
 
 class ConsumerBadgeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    consumer = serializers.CharField(max_length=128)
     badge = BadgeSerializer()
