3e5b7f5a036227cf76f49145234a327197f5e1f4
Nate Otto <nate@ottonomy.net>
2015-04-16 12:45:13 -0700

    BS-81 Implement PNG bakery as independent package

---
+++ apps/bakery/png_bakery.py
@@ -0,0 +1,62 @@
+import hashlib
+import itertools
+import os.path
+import png
+import re
+
+from django.conf import settings
+from django.core.files import File
+
+
+def unbake(imageFile):
+    """
+    Return the openbadges content contained in a baked PNG file.
+    If this doesn't work, return None.
+
+    If there is both an iTXt and tEXt chunk with keyword openbadges,
+    the iTXt chunk content will be returned.
+    """
+
+    reader = png.Reader(file=imageFile)
+    for chunktype, content in reader.chunks():
+        if chunktype == 'iTXt' and content.startswith('openbadges\x00'):
+            return re.sub('openbadges[\x00]+', '', content)
+        elif chunktype == 'tEXt' and content.startswith('openbadges\x00'):
+            return content.split('\x00')[1]
+
+
+def bake(imageFile, assertion_json_string):
+    """
+    Embeds a serialized representation of a badge instance in a PNG image file.
+    """
+    reader = png.Reader(file=imageFile)
+    filepath = os.path.join(
+        getattr(settings, 'MEDIA_ROOT'),
+        'uploads/badges/received/' + hashlib.md5(assertion_json_string).hexdigest() + '.png'
+        )
+    with open(filepath, 'w') as f:
+        newfile = File(f)
+        chunkheader = 'openbadges\x00\x00\x00\x00\x00'
+        badge_chunk = ('iTXt', bytes(chunkheader + assertion_json_string))
+        png.write_chunks(newfile, baked_chunks(reader.chunks(), badge_chunk))
+    newfile.close()
+    return newfile
+
+
+def baked_chunks(original_chunks, badge_chunk):
+    """
+    Returns an iterable of chunks that places the Open Badges baked chunk
+    and filters out any previous Open Badges chunk that may have existed.
+    """
+    def is_not_previous_assertion(chunk):
+        if chunk[1].startswith('openbadges\x00'):
+            return False
+        return True
+
+    first_slice = itertools.islice(original_chunks, 1)
+    last_slice = itertools.ifilter(
+        is_not_previous_assertion,
+        itertools.islice(original_chunks, 1, None)
+    )
+
+    return itertools.chain(first_slice, [badge_chunk], last_slice)
---
+++ apps/bakery/utils.py
@@ -0,0 +1,38 @@
+import re
+
+import png_bakery
+
+
+PNG_SIGNATURE = [137, 80, 78, 71, 13, 10, 26, 10]
+SVG_SIGNATURE = 'http://www.w3.org/2000/svg'
+HEADER_LENGTH = 256
+
+
+def check_image_type(imageFile):
+    header = imageFile.read(HEADER_LENGTH)
+    if [ord(char) for char in header[:len(PNG_SIGNATURE)]] == PNG_SIGNATURE:
+        return 'PNG'
+    elif re.compile(SVG_SIGNATURE).search(str(header)):
+        return 'SVG'
+
+
+def unbake(imageFile):
+    """
+    Return the openbadges content contained in a baked image.
+    """
+    image_type = check_image_type(imageFile)
+    if image_type == 'PNG':
+        return png_bakery.unbake(imageFile)
+    elif image_type == 'SVG':
+        raise NotImplementedError("SVG UNBAKING COMING SOON.")
+
+
+def bake(imageFile, assertion_json_string):
+    """
+    Embeds a serialized representation of a badge instance in an image file.
+    """
+    image_type = check_image_type(imageFile)
+    if image_type == 'PNG':
+        return png_bakery.bake(imageFile, assertion_json_string)
+    elif image_type == 'SVG':
+        raise NotImplementedError("SVG BAKING COMING SOON.")
