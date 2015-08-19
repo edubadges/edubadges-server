fa6a863fce381b71538ba69df379e1d8865cf590
Ian KP <iposs@concentricsky.com>
2015-08-19 11:14:02 -0700

    BS-174 Implement unsubscribe tracking system for issuer award emails

---
+++ apps/mainsite/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='EmailBlacklist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.EmailField(unique=True, max_length=75)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
---
+++ apps/mainsite/models.py
@@ -0,0 +1,35 @@
+import base64
+from datetime import datetime, timedelta
+from hashlib import sha1
+import hmac
+
+from django.conf import settings
+from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class EmailBlacklist(models.Model):
+    email = models.EmailField(unique=True)
+
+    @staticmethod
+    def generate_email_signature(email):
+        secret_key = settings.UNSUBSCRIBE_SECRET_KEY
+
+        expiration = datetime.utcnow() + timedelta(days=7)  # In one week.
+        timestamp = int((expiration - datetime(1970, 1, 1)).total_seconds())
+
+        email_encoded = base64.b64encode(email)
+        hashed = hmac.new(secret_key, email_encoded + str(timestamp), sha1)
+
+        return reverse('unsubscribe', kwargs={
+            'email_encoded': email_encoded,
+            'expiration': timestamp,
+            'signature': hashed.hexdigest(),
+        })
+
+    @staticmethod
+    def verify_email_signature(email_encoded, expiration, signature):
+        secret_key = settings.UNSUBSCRIBE_SECRET_KEY
+
+        hashed = hmac.new(secret_key, email_encoded + expiration, sha1)
+        return hmac.compare_digest(hashed.hexdigest(), str(signature))
