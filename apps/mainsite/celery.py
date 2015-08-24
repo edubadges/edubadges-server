cb45cf0bf77c0e010a995d7a97209722cf645d1d
Wiggins <wiggins@concentricsky.com>
2015-08-24 11:16:43 -0700

    celery support

---
+++ apps/mainsite/celery.py
@@ -0,0 +1,14 @@
+# Created by wiggins@concentricsky.com on 8/24/15.
+
+from __future__ import absolute_import
+
+from django.conf import settings
+from celery import Celery
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainsite.settings')
+app = Celery('mainsite')
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
