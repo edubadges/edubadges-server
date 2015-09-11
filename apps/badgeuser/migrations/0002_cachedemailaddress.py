d101b9ed6164acc50880cafb641cb83be0e31adf
Jeremy AAsum <jeremy@concentricsky.com>
2015-09-11 11:05:14 -0700

    Beginning of dialog

---
+++ apps/badgeuser/migrations/0002_cachedemailaddress.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0002_email_max_length'),
+        ('badgeuser', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CachedEmailAddress',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('account.emailaddress', models.Model),
+        ),
+    ]
