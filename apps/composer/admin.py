f1c1c8b72ab77a5f51a657af2bd72ce97c3f1d4c
Ian KP <iposs@concentricsky.com>
2015-09-11 11:37:21 -0700

    Remove from codebase packages refactored into/by the new composition package.

--- apps/composer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import (Collection, LocalBadgeInstanceCollection,)
-
-
-admin.site.register(Collection, admin.ModelAdmin)
-admin.site.register(LocalBadgeInstanceCollection, admin.ModelAdmin)
