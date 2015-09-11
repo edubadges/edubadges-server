fd3a8f23cc9f944ec97fdf7e4688248aab812b9e
Nate Otto <nate@ottonomy.net>
2015-09-11 15:40:13 -0700

    Revert GruntFile to avoid environment problem

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,85 +9,17 @@ module.exports = function(grunt) {
         
         pkg: grunt.file.readJSON('package.json'),
 
-        env: {
-            dist: {
-                NODE_ENV: 'production'
-            }
-        },
-
         watch: {
             options: {
-                spawn: false,
-                livereload: true
+                spawn: false
             },
             scss: {
                 files: '<%= srcPath %>**/*.scss',
                 tasks: ['sass', 'autoprefixer', 'bs-inject']
             },
             staticFiles: {
-                files: ['**/*.html'],
+                files: ['**/*.html','**/*.php'],
                 tasks: ['bs-inject']
-            },
-            browserify: {
-                files: [
-                    '<%= srcPath %>js/*.jsx',
-                    '<%= srcPath %>*.js',
-                    '<%= srcPath %>js/stores/*.jsx',
-                    '<%= srcPath %>js/actions/*.js',
-                    '<%= srcPath %>js/components/*.jsx',
-                ],
-                tasks: ['browserify:dev', 'bs-inject']
-            },
-            patterns: {
-                files: [
-                    '<%= srcPath %>js/pattern-library.html',
-                    '<%= srcPath %>js/patterns/*.jsx',
-                ],
-                tasks: ['browserify:patterns', 'inline:dist', 'bs-inject']
-            }
-        },
-
-        inline: {
-            dist: {
-                src: '<%= srcPath %>js/pattern-library.html',
-                dest: 'breakdown/templates/pattern-library.html'
-            }
-        },
-
-        browserify: {
-            dev: {
-                options: {
-                    debug: true,
-                    transform: ['reactify']
-                },
-                files: {
-                    'breakdown/static/js/app.js': 'build/js/app.jsx',
-                    'breakdown/static/js/lti-app.js': 'build/js/lti-app.jsx'
-                }
-            },
-            dist: {
-                options: {
-                    debug: false,
-                    transform: ['reactify']
-                },
-                files: {
-                    'breakdown/static/js/app.js': 'build/js/app.jsx',
-                    'breakdown/static/js/lti-app.js': 'build/js/lti-app.jsx'
-                }
-            },
-            patterns: {
-                options: {
-                    debug: true,
-                    transform: ['reactify']
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= srcPath %>js/patterns',
-                    src: ['*.pattern.jsx'],
-                    dest: '<%= srcPath %>js/temp',
-                    ext: '.pattern.js',
-                    extDot: 'first'
-                }]
             }
         },
 
@@ -99,13 +31,6 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= srcPath %>scss',
-                    src: ['*.scss', '!_*.scss'],
-                    dest: '<%= destPath %>css',
-                    ext: '.css'
-                },
-                {
-                    expand: true,
-                    cwd: '<%= srcPath %>scss/legacy',
                     src: ['*.scss', '!_*.scss'],
                     dest: '<%= destPath %>css',
                     ext: '.css'
@@ -134,17 +59,13 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask("bs-inject", function () {
-        browserSync.reload(['**/*.html','**/*.css']);
+        browserSync.reload(['**/*.html','**/*.php','**/*.css']);
     });
 
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-shell-spawn');
-    grunt.loadNpmTasks('grunt-inline');
-
     grunt.registerTask('default',['bs-init', 'watch']);
-    grunt.registerTask('dist', ['env:dist', 'sass', 'autoprefixer', 'browserify:dist']);
+
 }
