839dfde730b5584d669673db98abf7a1d8033561
Wiggins <wiggins@concentricsky.com>
2015-09-16 09:44:59 -0700

    Heading pattern component

---
+++ build/js/components/patterns/Heading.jsx
@@ -0,0 +1,31 @@
+var React = require('react')
+
+
+var Heading = React.createClass({
+    getDefaultProps: function() {
+        return {
+            'title': "Title",
+            'subtitle': undefined,
+            'size': undefined,
+        }
+    },
+
+    render: function() {
+        var subtitle = this.props.subtitle ? (<p>this.props.subtitle</p>) : "";
+        var size = this.props.size.toLowerCase();
+
+        if (size && (size in ["medium", "small"])) {
+            size = "heading_-"+size;
+        } else {
+            size = "";
+        }
+
+        return (
+            <header className="heading_ {size}">
+                <h1>{this.props.title}</h1>
+                {subtitle}
+            </header>);
+    }
+});
+
+module.exports = Heading;
