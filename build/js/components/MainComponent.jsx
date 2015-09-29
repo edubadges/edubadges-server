d429596dae441bf0377145e1b3cf980c0f15a2d7
Wiggins <wiggins@concentricsky.com>
2015-09-29 15:52:47 -0700

    Fix two scrollbars

--- build/js/components/MainComponent.jsx
+++ build/js/components/MainComponent.jsx
@@ -11,7 +11,7 @@ var MainComponent = React.createClass({
   render: function() {
     var spinner = <LoadingComponent/>
     return (
-      <div className="main-component clearfix">
+      <div>
         {this.props.dependenciesLoaded ? this.props.children : spinner}
       </div>
     );
