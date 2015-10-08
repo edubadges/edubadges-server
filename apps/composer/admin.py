0968a67207318b951b7a176105123956f4724539
Wiggins <wiggins@concentricsky.com>
2015-10-08 09:43:50 -0700

    admin cleanup - composition

--- apps/composer/admin.py
+++ apps/composer/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from .models import (Collection, StoredBadgeInstanceCollection,)
 
-
-admin.site.register(Collection, admin.ModelAdmin)
-admin.site.register(StoredBadgeInstanceCollection, admin.ModelAdmin)
+#deprecated
+#admin.site.register(Collection, admin.ModelAdmin)
+#admin.site.register(StoredBadgeInstanceCollection, admin.ModelAdmin)
