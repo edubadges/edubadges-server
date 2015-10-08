f6f909055a0d79e631d302a1fe6269435731872c
Wiggins <wiggins@concentricsky.com>
2015-10-08 08:45:21 -0700

    admin cleanup - badgeuser

--- apps/issuer/admin.py
+++ apps/issuer/admin.py
@@ -1,4 +1,3 @@
-import basic_models
 from django.contrib.admin import ModelAdmin
 from mainsite.admin import badgr_admin
 
@@ -7,26 +6,38 @@ from .models import Issuer, BadgeClass, BadgeInstance
 
 class IssuerAdmin(ModelAdmin):
     readonly_fields = ('created_at', 'created_by')
-    list_display = ('name', 'slug', 'created_by', 'created_at')
+    list_display = ('img', 'name', 'slug', 'created_by', 'created_at')
+    list_display_links = ('img', 'name')
     list_filter = ('created_at',)
     search_fields = ('name', 'slug')
     fieldsets = (
-        ('Meta', {'fields': ('created_by', 'created_at', 'owner'), 'classes': ("collapse",)}),
+        ('Metadata', {'fields': ('created_by', 'created_at', 'owner'), 'classes': ("collapse",)}),
         (None, {'fields': ('image', 'name', 'slug')}),
         ('JSON', {'fields': ('json',)}),
     )
+
+    def img(self, obj):
+        try:
+            return u'<img src="{}" width="32"/>'.format(obj.image.url)
+        except ValueError:
+            return obj.image
+    img.short_description = 'Image'
+    img.allow_tags = True
+
 badgr_admin.register(Issuer, IssuerAdmin)
 
 
 class BadgeClassAdmin(ModelAdmin):
     readonly_fields = ('created_at', 'created_by')
     list_display = ('badge_image', 'name', 'slug', 'issuer')
+    list_display_links = ('badge_image', 'name')
     list_filter = ('created_at',)
     search_fields = ('name', 'slug', 'issuer__name',)
     actions_on_top = True
     fieldsets = (
-        ('Meta', {'fields': ('created_by', 'created_at',), 'classes': ("collapse",)}),
-        (None, {'fields': ('image', 'name', 'slug', 'criteria_text')}),
+        ('Metadata', {'fields': ('created_by', 'created_at',), 'classes': ("collapse",)}),
+        (None, {'fields': ('image', 'name', 'slug', 'issuer')}),
+        ('Criteria', {'fields': ('criteria_text',)}),
         ('JSON', {'fields': ('json',)}),
     )
 
@@ -46,7 +57,7 @@ class BadgeInstanceAdmin(ModelAdmin):
     search_fields = ('slug', 'badgeclass__name', 'issuer__name')
     actions_on_top = True
     fieldsets = (
-        ('Meta', {'fields': ('created_by', 'created_at',), 'classes': ("collapse",)}),
+        ('Metadata', {'fields': ('created_by', 'created_at',), 'classes': ("collapse",)}),
         (None, {'fields': ('image', 'slug', 'recipient_identifier', 'badgeclass', 'issuer')}),
         ('Revocation', {'fields': ('revoked', 'revocation_reason')}),
         ('JSON', {'fields': ('json',)}),
