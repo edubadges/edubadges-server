a4e764b9af16315b4cde46efb32c79deae28150b
Ian KP <iposs@concentricsky.com>
2015-10-14 14:49:56 -0700

    BS-405 Make all images uploaded to be used for issuer or badge images square and resize down if necessary

--- apps/mainsite/mixins.py
+++ apps/mainsite/mixins.py
@@ -1,5 +1,6 @@
 import StringIO
 
+from django.conf import settings
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from PIL import Image
@@ -13,7 +14,17 @@ class ResizeUploadedImage(object):
             image = Image.open(self.image)
 
             if image.format == 'PNG':
-                new_image = resize_contain(image, (400, 400))
+                max_square = getattr(settings, 'IMAGE_FIELD_MAX_PX', 400)
+
+                smaller_than_canvas = \
+                    (image.width < max_square and image.height < max_square)
+
+                if smaller_than_canvas:
+                    max_square = (image.width
+                                  if image.width > image.height
+                                  else image.height)
+
+                new_image = resize_contain(image, (max_square, max_square))
 
                 byte_string = StringIO.StringIO()
                 new_image.save(byte_string, 'PNG')
