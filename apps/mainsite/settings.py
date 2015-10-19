bd91d06f8cc7b105df62dc108b61a5a4ebf2f5e0
Nate Otto <nate@ottonomy.net>
2015-10-19 10:33:19 -0700

    BS-594: Add TokenAuth to default auth classes for all rest framework API endpoints, not just issuer.

--- apps/mainsite/settings.py
+++ apps/mainsite/settings.py
@@ -330,6 +330,11 @@ REST_FRAMEWORK = {
         'mainsite.renderers.JSONLDRenderer',
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     )
 }
 
