45a4e9bd86323169918821e2484bf302f04dd9b9
Wiggins <wiggins@concentricsky.com>
2015-10-28 09:18:30 -0700

    have dist command fail if grunt fails

--- apps/mainsite/management/commands/dist.py
+++ apps/mainsite/management/commands/dist.py
@@ -1,5 +1,5 @@
 from django.core import management
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from subprocess import call
 
 
@@ -12,5 +12,7 @@ class Command(BaseCommand):
         Builds gulp resources.
         Gulp must be installed, and node_modules must be populated with `npm install`
         """
-        call(['grunt', 'dist'])
+        ret = call(['grunt', 'dist'])
+        if ret != 0:
+            raise CommandError("grunt dist failed")
         #management.call_command('test', verbosity=1)
