dc723a7ee183b69bb94b352d63bfc85a5b50ca64
Ian KP <iposs@concentricsky.com>
2015-12-28 13:51:08 -0800

    BS-809 UI: Create Collection - creating a collection with a duplicate name silently fails

--- apps/composition/api.py
+++ apps/composition/api.py
@@ -1,5 +1,7 @@
 import os
 
+from django.db import IntegrityError
+
 from rest_framework import permissions, status, serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -439,7 +441,11 @@ class CollectionList(APIView):
                                           context={'request': request})
 
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        try:
+            serializer.save()
+        except IntegrityError:
+            return Response("A collection with this name already exists",
+                            status=status.HTTP_400_BAD_REQUEST)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
