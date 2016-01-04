654dcf260c762d1ae9045c3dccd052705ae593b3
Nate Otto <nate@ottonomy.net>
2016-01-04 12:06:29 -0800

    BS-742: Add API-based user signup option

--- apps/badgeuser/tests.py
+++ apps/badgeuser/tests.py
@@ -1,3 +1,5 @@
+from django.core import mail
+
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIRequestFactory, APITestCase
 from badgeuser.models import BadgeUser
@@ -41,3 +43,46 @@ class AuthTokenTests(APITestCase):
 
         self.assertEqual(user.cached_token(), second_response.data.get('token'))
         self.assertEqual(Token.objects.get(user=user).key, user.cached_token())
+
+
+class UserCreateTests(APITestCase):
+    fixtures = ['0001_initial_superuser']
+
+    def test_create_user(self):
+        user_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'newuniqueuser1@example.com',
+            'password': '123456'
+        }
+
+        response = self.client.post('/v1/user/profile', user_data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(mail.outbox), 1)
+
+    def test_create_user_with_already_claimed_email(self):
+        user_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'test2@example.com',
+            'password': '123456'
+        }
+
+        response = self.client.post('/v1/user/profile', user_data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_can_create_user_with_preexisting_unconfirmed_email(self):
+        user_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'email': 'unclaimed1@example.com',
+            'password': '123456'
+        }
+
+        response = self.client.post('/v1/user/profile', user_data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(mail.outbox), 1)
