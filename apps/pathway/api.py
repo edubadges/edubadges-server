72d4c91052c14fb320b5876339815178afe2b3de
Wiggins <wiggins@concentricsky.com>
2016-04-01 10:39:29 -0700

    pathways - pathway element PUT and DELETE

--- apps/pathway/api.py
+++ apps/pathway/api.py
@@ -1,11 +1,11 @@
 # Created by wiggins@concentricsky.com on 3/30/16.
 from rest_framework import status
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 
 from issuer.api import AbstractIssuerAPIEndpoint
-from issuer.models import Issuer
+from issuer.models import Issuer, BadgeClass
 from pathway.models import Pathway, PathwayElement
 from pathway.serializers import PathwaySerializer, PathwayListSerializer, PathwayElementSerializer
 
@@ -180,20 +180,27 @@ class PathwayElementList(PathwayAPIEndpoint):
 
 class PathwayElementDetail(PathwayAPIEndpoint):
 
+    def _get_issuer_and_pathway_element(self, issuer_slug, pathway_slug, element_slug):
+        issuer, pathway = self._get_issuer_and_pathway(issuer_slug, pathway_slug)
+        if issuer is None or pathway is None:
+            return None, None, None
+
+        try:
+            element = PathwayElement.cached.get(slug=element_slug)
+        except PathwayElement.DoesNotExist:
+            return issuer, pathway, None
+        if element.pathway != pathway:
+            return issuer, pathway, None
+
+        return issuer, pathway, element
+
     def get(self, request, issuer_slug, pathway_slug, element_slug):
         """
         GET detail on a pathway, starting at a particular Pathway Element
         ---
         """
-        issuer, pathway = self._get_issuer_and_pathway(issuer_slug, pathway_slug)
-        if issuer is None or pathway is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        try:
-            element = PathwayElement.cached.get(slug=element_slug)
-        except PathwayElement.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        if element.pathway != pathway:
+        issuer, pathway, element = self._get_issuer_and_pathway_element(issuer_slug, pathway_slug, element_slug)
+        if issuer is None or pathway is None or element is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         serializer = PathwayElementSerializer(element, context={
@@ -203,3 +210,86 @@ class PathwayElementDetail(PathwayAPIEndpoint):
             'include_structure': True
         })
         return Response(serializer.data)
+
+    def put(self, request, issuer_slug, pathway_slug, element_slug):
+        """
+        Update a Pathway Element
+        ---
+        serializer: PathwayElementSerializer
+        parameters:
+            - name: parent
+              description: The slug of the parent Pathway Element to attach to
+              type: string
+              required: true
+              paramType: form
+            - name: name
+              description: The name of the Pathway Element
+              type: string
+              required: true
+              paramType: form
+            - name: description
+              description: The description of the Pathway Element
+              type: string
+              required: true
+              paramType: form
+            - name: ordering
+              description: The child order of this Pathway Element relative to its siblings
+              type: number
+              required: false
+              default: 99
+              paramType: form
+            - name: alignmentUrl
+              description: The external Alignment URL this Element aligns to
+              type: string
+              required: false
+              paramType: form
+            - name: completionBadge
+              description: The slug of the Badge Class to award when element is completed
+              type: string
+              required: false
+              paramType: form
+        """
+        issuer, pathway, element = self._get_issuer_and_pathway_element(issuer_slug, pathway_slug, element_slug)
+        if issuer is None or pathway is None or element is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        parent_element = None
+        try:
+            parent_element = PathwayElement.cached.get(slug=request.data.get('parent'))
+        except PathwayElement.DoesNotExist:
+            raise ValidationError("Invalid parent")
+
+        completion_badge = None
+        badge_slug = request.data.get('completionBadge', None)
+        if badge_slug:
+            try:
+                completion_badge = BadgeClass.cached.get(slug=badge_slug)
+            except BadgeClass.DoesNotExist:
+                raise ValidationError("Invalid completionBadge")
+
+        element.parent_element = parent_element
+        element.completion_badge = completion_badge
+        element.name = request.data.get('name')
+        element.description = request.data.get('description')
+        element.alignment_url = request.data.get('alignmentUrl')
+        element.ordering = int(request.data.get('ordering', 99))
+        element.save()
+        serializer = PathwayElementSerializer(element, context={
+            'request': request,
+            'issuer_slug': issuer_slug,
+            'pathway_slug': pathway_slug,
+            'include_structure': True
+        })
+        return Response(serializer.data)
+
+    def delete(self, request, issuer_slug, pathway_slug, element_slug):
+        """
+        Delete a Pathway Element
+        ---
+        """
+        issuer, pathway, element = self._get_issuer_and_pathway_element(issuer_slug, pathway_slug, element_slug)
+        if issuer is None or pathway is None or element is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        element.delete()
+        return Response(status=status.HTTP_200_OK)
