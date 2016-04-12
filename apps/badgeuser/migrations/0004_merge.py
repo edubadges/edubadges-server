97d9d67bd1b581290e846ee0b511badc385738cd
Wiggins <wiggins@concentricsky.com>
2016-04-12 08:07:58 -0700

    merge duplicate badgeuser migrations

---
+++ apps/badgeuser/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('badgeuser', '0003_auto_20160210_0313'),
+        ('badgeuser', '0003_auto_20160309_0820'),
+    ]
+
+    operations = [
+    ]
