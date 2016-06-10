3ff41d05e8e867736d927edadec7357803c88c33
Wiggins <wiggins@concentricsky.com>
2016-06-10 14:49:44 -0700

    BS-1635: include a public_url on BadgeInstanceSerializer thats based off of HTTP_ORIGIN

--- apps/issuer/renderers.py
+++ apps/issuer/renderers.py
@@ -1,6 +1,8 @@
+from django.core.urlresolvers import reverse
 from rest_framework.renderers import BrowsableAPIRenderer
 
 import utils
+from mainsite.utils import OriginSetting
 
 
 class BadgeInstanceHTMLRenderer(BrowsableAPIRenderer):
@@ -15,6 +17,9 @@ class BadgeInstanceHTMLRenderer(BrowsableAPIRenderer):
             context['badge_instance'] = renderer_context['badge_instance']
             context['badge_class'] = renderer_context['badge_class']
             context['issuer'] = renderer_context['issuer']
+            context['badge_instance_image_url'] = OriginSetting.HTTP+renderer_context['badge_instance'].image.url
+            context['badge_instance_public_url'] = OriginSetting.HTTP+reverse('badgeinstance_json', kwargs={
+                'slug': renderer_context['instance'].slug})
 
             recipient_email = renderer_context['badge_instance'].recipient_identifier
             context['obscured_recipient'] = utils.obscure_email_address(recipient_email)
