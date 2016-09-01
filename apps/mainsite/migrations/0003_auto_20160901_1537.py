29438a273622bfcd3dec290ed27b4fa3da543536
Nate Otto <nate@ottonomy.net>
2016-09-01 16:34:47 -0700

    Update Email blacklist model meta migration

---
+++ apps/mainsite/migrations/0003_auto_20160901_1537.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mainsite', '0002_badgrapp'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='emailblacklist',
+            options={'verbose_name': 'Blacklisted email', 'verbose_name_plural': 'Blacklisted emails'},
+        ),
+    ]
