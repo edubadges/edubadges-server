22b30ac0dc124474795815c4489790f6b0b33118
Wiggins <wiggins@concentricsky.com>
2016-10-06 11:38:43 -0700

    BS-554: expunge composer and credential_store. but preserve migrations

--- apps/composer/__init__.py
+++ apps/composer/__init__.py
@@ -1,6 +1,15 @@
 import warnings
 
 
+######
+#
+# !!! This app and all its models have been deprecated !!!
+# see the new 'composition' app
+#
+#####
+
+
+
 warnings.warn("The composer package is deprecated and will be removed \
             in a future release. Please remove it from your settings",
             DeprecationWarning, stacklevel=1)
---
+++ apps/credential_store/__init__.py
@@ -0,0 +1,13 @@
+import warnings
+
+######
+#
+# !!! This app and all its models have been deprecated !!!
+# see the new 'composition' app
+#
+#####
+
+
+warnings.warn("The credential_store package is deprecated and will be removed \
+            in a future release. Please remove it from your settings",
+            DeprecationWarning, stacklevel=1)
