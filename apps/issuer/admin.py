139383186008bf525c41d456f0f14d78768d6264
Nate Otto <nate@ottonomy.net>
2017-01-11 17:46:22 -0800

    Add action links to related objects in Issuer Admin

--- apps/issuer/admin.py
+++ apps/issuer/admin.py
@@ -1,10 +1,15 @@
 from django.contrib.admin import ModelAdmin
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+from django_object_actions import DjangoObjectActions
+
 from mainsite.admin import badgr_admin
 
 from .models import Issuer, BadgeClass, BadgeInstance
 
 
-class IssuerAdmin(ModelAdmin):
+class IssuerAdmin(DjangoObjectActions, ModelAdmin):
     readonly_fields = ('created_at', 'created_by')
     list_display = ('img', 'name', 'slug', 'created_by', 'created_at')
     list_display_links = ('img', 'name')
@@ -15,6 +20,7 @@ class IssuerAdmin(ModelAdmin):
         (None, {'fields': ('image', 'name', 'slug')}),
         ('JSON', {'fields': ('json',)}),
     )
+    change_actions = ['redirect_badgeclasses']
 
     def img(self, obj):
         try:
@@ -24,13 +30,20 @@ class IssuerAdmin(ModelAdmin):
     img.short_description = 'Image'
     img.allow_tags = True
 
+    def redirect_badgeclasses(self, request, obj):
+        return HttpResponseRedirect(
+            reverse('admin:issuer_badgeclass_changelist') + '?issuer__id={}'.format(obj.id)
+        )
+    redirect_badgeclasses.label = "BadgeClasses"
+    redirect_badgeclasses.short_description = "See this issuer's defined BadgeClasses"
+
 badgr_admin.register(Issuer, IssuerAdmin)
 
 
-class BadgeClassAdmin(ModelAdmin):
+class BadgeClassAdmin(DjangoObjectActions, ModelAdmin):
     readonly_fields = ('created_at', 'created_by')
-    list_display = ('badge_image', 'name', 'slug', 'issuer')
-    list_display_links = ('badge_image', 'name')
+    list_display = ('badge_image', 'name', 'slug', 'issuer_link', 'recipient_count')
+    list_display_links = ('badge_image', 'name',)
     list_filter = ('created_at',)
     search_fields = ('name', 'slug', 'issuer__name',)
     fieldsets = (
@@ -39,27 +52,47 @@ class BadgeClassAdmin(ModelAdmin):
         ('Criteria', {'fields': ('criteria_text',)}),
         ('JSON', {'fields': ('json',)}),
     )
+    change_actions = ['redirect_issuer', 'redirect_instances']
 
     def badge_image(self, obj):
         return u'<img src="{}" width="32"/>'.format(obj.image.url)
     badge_image.short_description = 'Badge'
     badge_image.allow_tags = True
 
+    def issuer_link(self, obj):
+        return '<a href="{}">{}</a>'.format(reverse("admin:issuer_issuer_change", args=(obj.issuer.id,)), obj.issuer.name)
+    issuer_link.allow_tags=True
+
+    def redirect_instances(self, request, obj):
+        return HttpResponseRedirect(
+            reverse('admin:issuer_badgeinstance_changelist') + '?badgeclass__id={}'.format(obj.id)
+        )
+    redirect_instances.label = "Instances"
+    redirect_instances.short_description = "See awarded instances of this BadgeClass"
+
+    def redirect_issuer(self, request, obj):
+        return HttpResponseRedirect(
+            reverse('admin:issuer_issuer_change', args=(obj.issuer.id,))
+        )
+    redirect_issuer.label = "Issuer"
+    redirect_issuer.short_description = "See this Issuer"
+
 badgr_admin.register(BadgeClass, BadgeClassAdmin)
 
 
-class BadgeInstanceAdmin(ModelAdmin):
+class BadgeInstanceAdmin(DjangoObjectActions, ModelAdmin):
     readonly_fields = ('created_at', 'created_by', 'image', 'slug')
     list_display = ('badge_image', 'recipient_identifier', 'slug', 'badgeclass', 'issuer')
     list_display_links = ('badge_image', 'recipient_identifier', )
     list_filter = ('created_at',)
-    search_fields = ('slug', 'badgeclass__name', 'issuer__name')
+    search_fields = ('recipient_identifier', 'slug', 'badgeclass__name', 'issuer__name')
     fieldsets = (
         ('Metadata', {'fields': ('created_by', 'created_at','acceptance'), 'classes': ("collapse",)}),
         (None, {'fields': ('image', 'slug', 'recipient_identifier', 'badgeclass', 'issuer')}),
         ('Revocation', {'fields': ('revoked', 'revocation_reason')}),
         ('JSON', {'fields': ('json',)}),
     )
+    change_actions = ['redirect_issuer', 'redirect_badgeclass']
 
     def badge_image(self, obj):
         try:
@@ -71,5 +104,19 @@ class BadgeInstanceAdmin(ModelAdmin):
 
     def has_add_permission(self, request):
         return False
+
+    def redirect_badgeclass(self, request, obj):
+        return HttpResponseRedirect(
+            reverse('admin:issuer_badgeclass_change', args=(obj.badgeclass.id,))
+        )
+    redirect_badgeclass.label = "BadgeClass"
+    redirect_badgeclass.short_description = "See this BadgeClass"
+
+    def redirect_issuer(self, request, obj):
+        return HttpResponseRedirect(
+            reverse('admin:issuer_issuer_change', args=(obj.issuer.id,))
+        )
+    redirect_issuer.label = "Issuer"
+    redirect_issuer.short_description = "See this Issuer"
 
 badgr_admin.register(BadgeInstance, BadgeInstanceAdmin)
