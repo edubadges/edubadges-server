d63058670d8e6843fc6474f80aa3a0df22d69ff4
Alex Weeks <aweeks@concentricsky.com>
2017-03-06 12:27:01 -0800

    Refactored CSVRenderer -> CSVDictRenderer.
    Also CSVDictRenderer.render() now properly handles dict() data parameter that is passed in when called as part of the exception handling code path in Django REST Framework.

--- apps/mainsite/renderers.py
+++ apps/mainsite/renderers.py
@@ -1,4 +1,6 @@
-from django.utils.encoding import smart_unicode
+import csv
+import StringIO
+
 from rest_framework import renderers
 
 
@@ -10,9 +12,31 @@ class JSONLDRenderer(renderers.JSONRenderer):
     format = 'ld+json'
 
 
-class CSVRenderer(renderers.BaseRenderer):
+class CSVDictRenderer(renderers.BaseRenderer):
     media_type = 'text/csv'
     format = 'csv'
 
     def render(self, data, media_type=None, renderer_context=None):
-        return data.encode(self.charset)
+        response = renderer_context.get('response', None)
+
+        if response is not None and response.exception:
+            return None
+
+            # TODO: Replace return None with commented lines below to get human-readable description in response
+            #
+            # Returning a non-None value in this function, as part of the exception handling code path in Django REST
+            # Framework, generates an HTTP response that Chrome rejects as invalid (ERR_INVALID_RESPONSE).  Possible
+            # Django REST Framework bug?
+            #
+            # fieldnames = data.keys()
+            # rows = [data]
+        else:
+            fieldnames = data['fieldnames']
+            rows = data['rowdicts']
+
+        buff = StringIO.StringIO()
+        writer = csv.DictWriter(buff, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+
+        return buff.getvalue().encode(self.charset)
