0016207946222b76b17d3827df521de32ec15db0
Wiggins <wiggins@concentricsky.com>
2017-05-09 14:35:47 -0700

    handle ProtectedError for /v1/ also

--- apps/entity/views.py
+++ apps/entity/views.py
@@ -13,6 +13,9 @@ def exception_handler(exc, context):
     version = context.get('kwargs', None).get('version', 'v1')
     if version == 'v1':
         # Use the default exception-handling logic for v1
+        if isinstance(exc, ProtectedError):
+            description, protected_objects = exc.args
+            return Response(description, response_code=status.HTTP_400_BAD_REQUEST)
         return views.exception_handler(exc, context)
     elif version == 'v2':
         description = 'miscellaneous error'
