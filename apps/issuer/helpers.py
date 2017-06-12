09c45fcc0115e0e3adba298cb3a9b401b71242ac
Wiggins <wiggins@concentricsky.com>
2017-06-12 08:15:44 -0700

    Django cache requests_cache backend

--- apps/issuer/helpers.py
+++ apps/issuer/helpers.py
@@ -1,23 +1,120 @@
 # encoding: utf-8
 from __future__ import unicode_literals
 
-import badgecheck
+import uuid
+from collections import MutableMapping
+
+from django.core.cache import cache
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import transaction
+from requests_cache.backends import RedisCache, BaseCache
 
 from issuer.models import Issuer, BadgeClass, BadgeInstance
 
 
+# This import order matters, Don't change it! [Wiggins]
+import requests_cache
+requests_cache.install_cache()
+import badgecheck
+
+
+class DjangoCacheDict(MutableMapping):
+    _keymap_cache_key = "DjangoCacheDict_keys"
+
+    def __init__(self, namespace, id=None, timeout=None):
+        self.namespace = namespace
+        self._timeout = timeout
+
+        if id is None:
+            id = uuid.uuid4().hexdigest()
+        self._id = id
+        self.keymap_cache_key = self._keymap_cache_key+"_"+self._id
+
+    def build_key(self, *args):
+        return "{keymap_cache_key}{namespace}{key}".format(
+            keymap_cache_key=self.keymap_cache_key,
+            namespace=self.namespace,
+            key="".join(args)
+        ).encode("utf-8")
+
+    def timeout(self):
+        return self._timeout
+
+    def _keymap(self):
+        keymap = cache.get(self.keymap_cache_key)
+        if keymap is None:
+            return []
+        return keymap
+
+    def __getitem__(self, key):
+        result = cache.get(self.build_key(key))
+        if result is None:
+            raise KeyError
+        return result
+
+    def __setitem__(self, key, value):
+        built_key = self.build_key(key)
+        cache.set(built_key, value, timeout=self.timeout())
+
+        # this probably needs locking...
+        keymap = self._keymap()
+        keymap.append(built_key)
+        cache.set(self.keymap_cache_key, keymap, timeout=None)
+
+    def __delitem__(self, key):
+        built_key = self.build_key(key)
+        cache.delete(built_key)
+
+        # this probably needs locking...
+        keymap = self._keymap()
+        keymap.remove(built_key)
+        cache.set(self.keymap_cache_key, keymap, timeout=None)
+
+    def __len__(self):
+        keymap = self._keymap()
+        return len(keymap)
+
+    def __iter__(self):
+        keymap = self._keymap()
+        for key in keymap:
+            yield cache.get(key)
+
+    def __str__(self):
+        return '<{}>'.format(self.keymap_cache_key)
+
+    def clear(self):
+        self._id = uuid.uuid4().hexdigest()
+        self.keymap_cache_key = self._keymap_cache_key+"_"+self._id
+
+
+class DjangoCacheRequestsCacheBackend(BaseCache):
+    def __init__(self, namespace='requests-cache', **options):
+        super(DjangoCacheRequestsCacheBackend, self).__init__(**options)
+        self.responses = DjangoCacheDict(namespace, 'responses')
+        self.keys_map = DjangoCacheDict(namespace, 'urls')
+
+
 class BadgeCheckHelper(object):
+    _cache_instance = None
+
+    @classmethod
+    def cache_instance(cls):
+        if cls._cache_instance is None:
+            cls._cache_instance = DjangoCacheRequestsCacheBackend(namespace='badgr_requests_cache')
+        return cls._cache_instance
+
     @classmethod
     def badgecheck_options(cls):
         return getattr(settings, 'BADGECHECK_OPTIONS', {
-            'include_original_json': True
+            'include_original_json': True,
+            'use_cache': True,
+            'cache_backend': cls.cache_instance()
         })
 
     @classmethod
     def get_or_create_assertion(cls, url=None, imagefile=None, assertion=None, created_by=None):
+
         # distill 3 optional arguments into one query argument
         query = (url, imagefile, assertion)
         query = filter(lambda v: v is not None, query)
@@ -32,7 +129,7 @@ class BadgeCheckHelper(object):
         else:
             badgecheck_recipient_profile = None
 
-        response = badgecheck.verify(query, recipient_profile=badgecheck_recipient_profile, options=cls.badgecheck_options())
+        response = badgecheck.verify(query, recipient_profile=badgecheck_recipient_profile, **cls.badgecheck_options())
         is_valid = response.get('valid')
 
         # we expect to get 3 obos: Assertion, Issuer and BadgeClass
