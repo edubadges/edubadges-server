9cf0c2f79ce92a4dcba6ddbccd06d6f814146b34
Wiggins <wiggins@concentricsky.com>
2017-06-21 11:22:38 -0700

    BGR-259: redirect legacy /share/badge urls to public assertion page

--- apps/backpack/share_urls.py
+++ apps/backpack/share_urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
-from backpack.views import SharedCollectionView, EmbeddedSharedCollectionView
+from backpack.views import SharedCollectionView, EmbeddedSharedCollectionView, LegacyBadgeShareRedirectView
 
 urlpatterns = [
     url(r'^/collection/(?P<share_hash>[^/]+)$', SharedCollectionView.as_view(), name='backpack_shared_collection'),
     url(r'^/collection/(?P<share_hash>[^/]+)/embed$', EmbeddedSharedCollectionView.as_view(), name='backpack_shared_collection_embed'),
+
+    # legacy redirects
+    url(r'^/badge/(?P<share_hash>[^/]+)$', LegacyBadgeShareRedirectView.as_view(), name='legacy_redirect_backpack_shared_badge'),
 ]
 
