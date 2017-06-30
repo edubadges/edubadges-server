43ef211326a0bdfa38ee62b72fdf31817b1a6819
Alex Weeks <aweeks@concentricsky.com>
2017-06-30 13:32:06 -0700

    Added cancel login view

--- apps/badgrsocialauth/urls.py
+++ apps/badgrsocialauth/urls.py
@@ -2,10 +2,14 @@ from allauth.socialaccount import providers
 from django.conf.urls import url
 from django.utils import importlib
 
-from badgrsocialauth.views import BadgrSocialLogin, BadgrSocialEmailExists, BadgrSocialAccountVerifyEmail
+from badgrsocialauth.views import BadgrSocialLogin, BadgrSocialEmailExists, BadgrSocialAccountVerifyEmail, \
+    BadgrSocialLoginCancel
 
 urlpatterns = [
     url(r'^sociallogin', BadgrSocialLogin.as_view(permanent=False)),
+
+    # Intercept allauth cancel login view
+    url(r'^cancellogin', BadgrSocialLoginCancel.as_view(permanent=False), name='socialaccount_login_cancelled'),
 
     # Intercept allauth connect view (if account with given email already exists) and redirect to UI
     url(r'^emailexists', BadgrSocialEmailExists.as_view(permanent=False), name='socialaccount_signup'),
