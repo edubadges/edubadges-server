e3f4849552f97e7e84808c271ff9be2150f77d31
Wiggins <wiggins@concentricsky.com>
2018-08-06 15:20:23 -0700

    use email from azure extra_data if needed
    refs BGR-1294

--- apps/badgrsocialauth/serializers.py
+++ apps/badgrsocialauth/serializers.py
@@ -11,11 +11,14 @@ class BadgrSocialAccountSerializerV1(serializers.Serializer):
         representation = super(BadgrSocialAccountSerializerV1, self).to_representation(instance)
         provider = instance.get_provider()
         common_fields = provider.extract_common_fields(instance.extra_data)
+        email = common_fields.get('email', None)
+        if not email and 'userPrincipalName' in instance.extra_data:
+            email = instance.extra_data['userPrincipalName']
 
         representation.update({
             'firstName': common_fields.get('first_name', None),
             'lastName': common_fields.get('last_name', None),
-            'primaryEmail': common_fields.get('email', None),
+            'primaryEmail': email,
         })
 
         return representation
