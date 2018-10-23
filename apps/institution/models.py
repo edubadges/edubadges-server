6ab0515e2f0b45dc8fd1f728e3fd976ad9c35e2f
smoquet <you@example.com>
2018-10-23 16:10:51 +0200

    adds plural name for faculty

--- apps/institution/models.py
+++ apps/institution/models.py
@@ -12,6 +12,9 @@ class Faculty(models.Model):
     
     def __str__(self):
         return self.name
+    
+    class Meta:    
+        verbose_name_plural = 'faculties'
 
     name = models.CharField(max_length=512)
     institution = models.ForeignKey(Institution, blank=False, null=False)
