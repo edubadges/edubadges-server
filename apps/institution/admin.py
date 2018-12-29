3bac73779b05b4ee579503ff710be200ac3c65eb
smoquet <you@example.com>
2018-12-29 15:37:26 +0100

    auto fills institution when creating faculty in admin

--- apps/institution/admin.py
+++ apps/institution/admin.py
@@ -8,13 +8,22 @@ class InstitutionAdmin(admin.ModelAdmin):
 
 class FacultyAdmin(FilterByScopeMixin, admin.ModelAdmin):
     
+    list_display = ('name', 'institution')
+    
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(FacultyAdmin, self).get_form(request, obj, **kwargs)
+        institution = request.user.institution
+        if institution:
+            form.base_fields['institution'].initial = institution.id
+        return form
+    
     def filter_queryset_institution(self, queryset, request):
         institution_id = request.user.institution.id
         return queryset.filter(institution_id=institution_id).distinct()
-    
+     
     def filter_queryset_faculty(self, queryset, request):
         return queryset.filter(faculty__in=request.user.faculty.all()).distinct()
-    
+     
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "institution":
             if not request.user.is_superuser:
