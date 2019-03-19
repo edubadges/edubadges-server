09e74a3282c8e89432f506c994314b554ec52a2f
daan <daniel@bitofpepper.com>
2019-03-19 09:41:55 +0100

    login not wroking weird bug

--- apps/lti_edu/lti_views.py
+++ apps/lti_edu/lti_views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import TemplateView
 
+from badgrsocialauth.utils import set_session_badgr_app
+from mainsite.models import BadgrApp
+
 
 class LoginLti(TemplateView):
     template_name = "lti/test_lti.html"
@@ -9,7 +12,9 @@ class LoginLti(TemplateView):
     def get_context_data(self, **kwargs):
         context_data = super(LoginLti, self).get_context_data(**kwargs)
         context_data['ltitest'] = 'yes lti test'
-
+        badgr_app = BadgrApp.objects.get_current(request=self.request)
+        if badgr_app is not None:
+            set_session_badgr_app(self.request, badgr_app)
         if not self.request.user.is_authenticated():
             context_data['login_url'] = reverse('edu_id_login')
         return context_data
