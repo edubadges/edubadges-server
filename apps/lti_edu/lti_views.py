8413d73d7d8a77fddae25bffbc383a3c49c95a36
daan <daniel@bitofpepper.com>
2019-05-03 10:55:58 +0200

    fixing badgr app bug

--- apps/lti_edu/lti_views.py
+++ apps/lti_edu/lti_views.py
@@ -50,7 +50,7 @@ class LoginLti(TemplateView):
     def get_context_data(self, **kwargs):
         context_data = super(LoginLti, self).get_context_data(**kwargs)
         context_data['ltitest'] = 'yes lti test'
-        badgr_app = BadgrApp.objects.get_current(request=self.request)
+        badgr_app = kwargs['tenant'].badgr_app
         context_data['login_url'] = self.get_login_url()
         if badgr_app is not None:
             set_session_badgr_app(self.request, badgr_app)
@@ -89,7 +89,8 @@ class LoginLti(TemplateView):
 
     def get_after_login(self):
         badgr_app = BadgrApp.objects.get_current(request=self.request)
-        return '{}/lti-badges?embedVersion=1&embedWidth=800&embedHeight=800'.format(badgr_app.cors)
+        scheme = self.request.is_secure() and "https" or "http"
+        return '{}://{}/lti-badges?embedVersion=1&embedWidth=800&embedHeight=800'.format(scheme,badgr_app.cors)
 
     def get_check_login_url(self):
         return reverse('check-login')
@@ -102,8 +103,9 @@ class LoginLtiStaff(LoginLti):
         return reverse('surf_conext_login')
 
     def get_after_login(self):
+        scheme = self.request.is_secure() and "https" or "http"
         badgr_app = BadgrApp.objects.get_current(request=self.request)
-        return '{}/issuer?embedVersion=1&embedWidth=800&embedHeight=800'.format(badgr_app.cors)
+        return '{}://{}/issuer?embedVersion=1&embedWidth=800&embedHeight=800'.format(scheme, badgr_app.cors)
 
     def get_check_login_url(self):
         return reverse('check-login-staff')
