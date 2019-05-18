f5de309f26f40b473e4e26116dc4e874317c20b2
smoquet <you@example.com>
2019-05-18 16:30:34 +0200

    adds statistics endpoints

--- apps/management/api.py
+++ apps/management/api.py
@@ -1,7 +1,8 @@
-from entity.api import BaseEntityListView
-from mainsite.permissions import AuthenticatedWithVerifiedEmail, MayUseManagementDashboard, ObjectWithinUserScope
 from django.contrib.auth.models import Group
-from management.serializers import GroupSerializer
+from entity.api import BaseEntityListView, BaseEntityDetailView
+from mainsite.permissions import AuthenticatedWithVerifiedEmail, MayUseManagementDashboard, ObjectWithinUserScope
+from management.serializers import GroupSerializer, FacultySerializerStatistics, BadgeClassSerializerStatistics, IssuerSerializerStatistics
+from institution.models import Faculty
 
 
 class GroupList(BaseEntityListView):
@@ -15,3 +16,14 @@ class GroupList(BaseEntityListView):
 
     def get(self, request, **kwargs):
         return super(GroupList, self).get(request, **kwargs)
+
+
+class FacultyStats(BaseEntityDetailView):
+
+    model = Faculty
+    http_method_names = ['get']
+    permission_classes = (AuthenticatedWithVerifiedEmail, MayUseManagementDashboard, ObjectWithinUserScope)
+    serializer_class = FacultySerializerStatistics
+
+    def get(self, request, **kwargs):
+        return super(FacultyStats, self).get(request, **kwargs)
