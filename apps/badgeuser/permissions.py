fa8e7182abfbcd8dbbbceaf0dc1321a0b9258c61
smoquet <you@example.com>
2019-06-09 18:48:01 +0200

    adds must be teacher permission to, and removes faculty object json dumps from, issuer endpoints

--- apps/badgeuser/permissions.py
+++ apps/badgeuser/permissions.py
@@ -11,3 +11,8 @@ class BadgeUserIsAuthenticatedUser(BasePermission):
         if isinstance(obj, BadgeUser):
             return request.user.pk == obj.pk
         return False
+
+
+class BadgeUserHasSurfconextSocialAccount(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_surf_conext_social_account()
