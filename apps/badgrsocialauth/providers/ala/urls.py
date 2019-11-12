a7c6240fd93154266dce88baf0b74259ea128a31
daan <daniel@bitofpepper.com>
2019-11-12 16:05:27 +0100

    started ala implementation

---
+++ apps/badgrsocialauth/providers/ala/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+
+    # note: in registering the badgr app at SurfConext, the callback url is explicitely registered
+    url('^eduid/login/callback/$', views.callback, name='edu_id_callback'),
+    url('^eduid/login/$', views.login, name="edu_id_login"),
+    url('^eduid/login/terms_accepted/(?P<state>[^/]+)/(?P<access_token>[^/]+)', views.after_terms_agreement, name="eduid_terms_accepted_callback")
+]
