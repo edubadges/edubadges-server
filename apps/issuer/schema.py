55a8ceee16dfb44d040c2053c98cd42ef2d595fc
Malou <malou_allertz@hotmail.com>
2020-02-24 17:57:21 +0100

    use graphql to query issuers and badgeclasses

---
+++ apps/issuer/schema.py
@@ -0,0 +1,38 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+from .models import Issuer, BadgeClass
+
+class IssuerType(DjangoObjectType):
+    class Meta:
+        model = Issuer
+
+class BadgeClassType(DjangoObjectType):
+    class Meta:
+        model = BadgeClass
+
+class Query(object):
+    all_issuers = graphene.List(IssuerType)
+    def resolve_all_issuers(self, info, **kwargs):
+        return Issuer.objects.all()
+
+    issuer = graphene.Field(IssuerType, id=graphene.ID())
+    def resolve_issuer(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Issuer.objects.get(id=id)
+
+        return None
+
+    all_badge_classes = graphene.List(BadgeClassType)
+    def resolve_all_badge_classes(self, info, **kwargs):
+        return BadgeClass.objects.all()
+
+    badge_class = graphene.Field(BadgeClassType, id=graphene.ID())
+    def resolve_badge_class(self, info, **kwargs):
+        id =  kwargs.get('id')
+
+        if id is not None:
+            return BadgeClass.objects.get(id=id)
+
+        return None
--- apps/mainsite/schema.py
+++ apps/mainsite/schema.py
@@ -1,8 +1,9 @@
 import graphene
 
 import institution.schema
+import issuer.schema
 
-class Query(institution.schema.Query, graphene.ObjectType):
+class Query(institution.schema.Query, issuer.schema.Query, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query)
