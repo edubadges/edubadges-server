1a87b181ba8487b3177d88261748f7e2a033f006
smoquet <you@example.com>
2020-03-04 17:29:32 +0100

    staff can now query users again

--- apps/mainsite/schema.py
+++ apps/mainsite/schema.py
@@ -2,6 +2,7 @@ import graphene
 
 import institution.schema
 import issuer.schema
+from badgeuser import schema  # unused import is needed, without it staff cannot query users
 
 class Query(institution.schema.Query,
             issuer.schema.Query,
