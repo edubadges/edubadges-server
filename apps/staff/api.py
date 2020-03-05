92f731904309ddc26e70619476a87e044df25007
smoquet <you@example.com>
2020-03-05 15:33:19 +0100

    implements permissioned staff creation rest endpoints

--- apps/staff/api.py
+++ apps/staff/api.py
@@ -1,44 +1,56 @@
-from entity.api import BaseEntityListView
-from institution.models import Faculty
+from entity.api import BaseEntityListView, VersionedObjectMixin
+from institution.models import Faculty, Institution
 from issuer.models import Issuer, BadgeClass
 from mainsite.permissions import AuthenticatedWithVerifiedEmail
-from staff.serializers import StaffSerializer
+from staff.serializers import BadgeClassStaffSerializer, IssuerStaffSerializer, FacultyStaffSerializer, InstitutionStaffSerializer
 from staff.permissions import HasObjectPermission
 
 
 class StaffListViewMixin(object):
     allowed_methods = ('POST',)
-    permission_map = {'POST': 'administrate_users'}
-    serializer_class = StaffSerializer
+    permission_map = {'POST': 'may_administrate_users'}
+
     permission_classes = (AuthenticatedWithVerifiedEmail, HasObjectPermission)
 
     def post(self, request, **kwargs):
         """
         create a new staff membership
         """
-        self.get_object(request, **kwargs)  # trigger a has_object_permissions() check
+        object = self.get_object(request, **kwargs)  # trigger a has_object_permissions() check on the model instance
         return super(StaffListViewMixin, self).post(request, **kwargs)
 
 
-class FacultyStaffList(StaffListViewMixin, BaseEntityListView):
+class InstitutionStaffList(VersionedObjectMixin, StaffListViewMixin, BaseEntityListView):
     """
     Get staff members you may administrate within a Faculty context
     Post to add a new staff member within Faculty context
     """
-    model = Faculty  # used by get_object()
+    model = Institution  # used by get_object()
+    serializer_class = InstitutionStaffSerializer
 
 
-class IssuerStaffList(StaffListViewMixin, BaseEntityListView):
+class FacultyStaffList(VersionedObjectMixin, StaffListViewMixin, BaseEntityListView):
+    """
+    Get staff members you may administrate within a Faculty context
+    Post to add a new staff member within Faculty context
+    """
+    model = Faculty
+    serializer_class = FacultyStaffSerializer
+
+
+class IssuerStaffList(VersionedObjectMixin, StaffListViewMixin, BaseEntityListView):
     """
     Get staff members you may administrate within an Issuer context
     Post to add a new staff member within Issuer context
     """
-    model = Issuer  # used by get_object()
+    model = Issuer
+    serializer_class = IssuerStaffSerializer
 
 
-class BadgeClassStaffList(StaffListViewMixin, BaseEntityListView):
+class BadgeClassStaffList(VersionedObjectMixin, StaffListViewMixin, BaseEntityListView):
     """
     Get staff members you may administrate within an Issuer context
     Post to add a new staff member within Issuer context
     """
-    model = BadgeClass  # used by get_object()
+    model = BadgeClass
+    serializer_class = BadgeClassStaffSerializer
--- apps/staff/api_urls.py
+++ apps/staff/api_urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from staff.api import FacultyStaffList, IssuerStaffList, BadgeClassStaffList
+from staff.api import InstitutionStaffList, FacultyStaffList, IssuerStaffList, BadgeClassStaffList
 
 urlpatterns = [
-    url(r'^faculty/(?P<slug>[^/]+)', FacultyStaffList.as_view(), name='faculty_staff_list'),
-    url(r'^issuer/(?P<slug>[^/]+)$', IssuerStaffList.as_view(), name='issuer_staff_list'),
-    url(r'^badgeclass/(?P<slug>[^/]+)$', BadgeClassStaffList.as_view(), name='badgeclass_staff_list')
+    url(r'^institution/(?P<slug>[^/]+)/create$', InstitutionStaffList.as_view(), name='faculty_staff_list'),
+    url(r'^faculty/(?P<slug>[^/]+)/create$', FacultyStaffList.as_view(), name='faculty_staff_list'),
+    url(r'^issuer/(?P<slug>[^/]+)/create$', IssuerStaffList.as_view(), name='issuer_staff_list'),
+    url(r'^badgeclass/(?P<slug>[^/]+)/create$', BadgeClassStaffList.as_view(), name='badgeclass_staff_list')
     ]
 
