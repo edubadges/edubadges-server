626ac4331c555099871b3fa152c4fd59f7262a7e
smoquet <you@example.com>
2020-03-05 20:17:25 +0100

    removes unused baseentity

--- apps/entity/models.py
+++ apps/entity/models.py
@@ -1,6 +1,4 @@
 # encoding: utf-8
-
-
 import cachemodel
 from django.db import models
 
@@ -32,41 +30,6 @@ class _AbstractVersionedEntity(cachemodel.CacheModel):
         return super(_AbstractVersionedEntity, self).delete(*args, **kwargs)
 
 
-class _MigratingToBaseVersionedEntity(_AbstractVersionedEntity):
-    """
-    A temporary abstract model that exists to provide a migration path for existing models to BaseVersionedEntity.
-
-    Usage:
-       1.) change ExistingModel to subclass from _MigratingToBaseVersionedEntity
-       2.) ./manage.py makemigrations existing_app  # existing_app.ExistingModel should get a migration that adds entity fields, default=None
-       3.) ./manage.py makemigrations existing_app --empty  # build a data migration that will populate the new fields:
-       example:
-            operations = [
-                entity.db.migrations.PopulateEntityIdsMigration('existing_app', 'ExistingModel'),
-            ]   
-       4.) change ExistingModel to subclass from BaseVersionedEntity instead of _MigratingToBaseVersionedEntity
-       5.) ./manage.py makemigrations existing_app  # make migration that sets unique=True 
-        
-    """
-    entity_id = models.CharField(max_length=254, blank=False, null=True, default=None)
-
-    class Meta:
-        abstract = True
-
-
-class BaseEntity(cachemodel.CacheModel):
-
-    class Meta:
-        abstract = True
-
-    entity_id = models.CharField(max_length=254, unique=True, default=None)  # default=None is required
-
-    def save(self, *args, **kwargs):
-        if self.entity_id is None:
-            self.entity_id = generate_entity_uri()
-        return super(BaseEntity, self).save(*args, **kwargs)
-
-
 class BaseVersionedEntity(_AbstractVersionedEntity):
     entity_id = models.CharField(max_length=254, unique=True, default=None)  # default=None is required
 
@@ -78,3 +41,4 @@ class BaseVersionedEntity(_AbstractVersionedEntity):
             return '{}{}'.format(type(self)._meta.object_name, self.entity_id)
         except AttributeError:
             return self.entity_id
+
