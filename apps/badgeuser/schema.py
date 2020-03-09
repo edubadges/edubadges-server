966592e203ac841dbc19225732bf2d62746fcd4f
Malou <malou_allertz@hotmail.com>
2020-03-09 10:37:23 +0100

    graphql: retrieve current user with institution

--- apps/badgeuser/schema.py
+++ apps/badgeuser/schema.py
@@ -1,10 +1,19 @@
+import graphene
 from graphene_django.types import DjangoObjectType
 from badgeuser.models import BadgeUser
-
+from institution.schema import InstitutionType
 
 class BadgeUserType(DjangoObjectType):
 
     class Meta:
         model = BadgeUser
-        fields = ('first_name', 'last_name', 'email', 'entity_id',)
+        fields = ('first_name', 'last_name', 'email', 'entity_id')
 
+    institution = graphene.Field(InstitutionType)
+
+
+class Query(object):
+    current_user = graphene.Field(BadgeUserType)
+
+    def resolve_current_user(self, info, **kwargs):
+        return info.context.user
--- apps/mainsite/schema.py
+++ apps/mainsite/schema.py
@@ -2,11 +2,9 @@ import graphene
 
 import institution.schema
 import issuer.schema
-from badgeuser import schema  # unused import is needed, without it staff cannot query users
+import badgeuser.schema
 
-class Query(institution.schema.Query,
-            issuer.schema.Query,
-            graphene.ObjectType):
+class Query(institution.schema.Query, issuer.schema.Query, badgeuser.schema.Query, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query)
