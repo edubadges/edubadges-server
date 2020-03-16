3d53b005c64d31c34052c9be94b42e5f347044c3
Okke Harsta <oharsta@zilverline.com>
2020-03-16 16:15:27 +0100

    Commented out faulty migration

--- apps/issuer/migrations/0056_populate_badgeinstance_user.py
+++ apps/issuer/migrations/0056_populate_badgeinstance_user.py
@@ -2,24 +2,28 @@ from django.db import migrations
 
 
 def populate_badgeinstance_user(apps, schema_editor):
-    BadgeInstance = apps.get_model('issuer', 'BadgeInstance')
-    SocialAccount = apps.get_model('socialaccount', 'SocialAccount')
-    for instance in BadgeInstance.objects.all():
-        identifier = instance.recipient_identifier
-        if identifier.startswith('urn:mace:eduid'):
-            try:
-                account = SocialAccount.objects.get(uid=identifier)
-                instance.user = account.user
-                instance.save()
-            except SocialAccount.DoesNotExist:
-                pass
+    # Does not work and instead of faking we for now skip this migration
+    pass
+
+
+    # BadgeInstance = apps.get_model('issuer', 'BadgeInstance')
+    # SocialAccount = apps.get_model('socialaccount', 'SocialAccount')
+    # for instance in BadgeInstance.objects.all():
+    #     identifier = instance.recipient_identifier
+    #     if identifier.startswith('urn:mace:eduid'):
+    #         try:
+    #             account = SocialAccount.objects.get(uid=identifier)
+    #             instance.user = account.user
+    #             instance.save()
+    #         except SocialAccount.DoesNotExist:
+    #             pass
+
 
 def noop(apps, schema_editor):
     pass
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('issuer', '0055_badgeinstance_user'),
     ]
