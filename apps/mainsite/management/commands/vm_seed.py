41be495286a95392cb35351d18aba61336c101df
Malou <malou_allertz@hotmail.com>
2020-03-17 13:16:23 +0100

    separate command for seeds on vm

---
+++ apps/mainsite/management/commands/vm_seed.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+import traceback
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            __import__("mainsite.seeds.01_setup")
+            print("\033[92mdone!\033[0m")
+        except:
+            print("\033[91mFAILED!\033[0m")
+            traceback.print_exc()
