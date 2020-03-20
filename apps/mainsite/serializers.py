20876b1d357fd31f8a57f50d889980e4265236ff
smoquet <you@example.com>
2020-03-20 13:10:44 +0100

    removes unused mainsite serializers

--- apps/mainsite/serializers.py
+++ apps/mainsite/serializers.py
@@ -23,26 +23,6 @@ class BadgrBaseModelSerializer(serializers.ModelSerializer):
             raise BadgrValidationError(fields=e.detail)
 
 
-class ReadOnlyJSONField(serializers.CharField):
-
-    def to_representation(self, value):
-        if isinstance(value, (dict, list)):
-            return value
-        else:
-            raise serializers.ValidationError("WriteableJsonField: Did not get a JSON-serializable datatype from storage for this item: " + str(value))
-
-
-class WritableJSONField(ReadOnlyJSONField):
-    def to_internal_value(self, data):
-        try:
-            internal_value = json.loads(data)
-        except Exception:
-            # TODO: this is going to choke on dict input, when it should be allowed in addition to JSON.
-            raise serializers.ValidationError("WriteableJsonField: Could not process input into a python dict for storage " + str(data))
-
-        return internal_value
-
-
 class LinkedDataEntitySerializer(serializers.Serializer):
     def to_representation(self, instance):
         representation = super(LinkedDataEntitySerializer, self).to_representation(instance)
