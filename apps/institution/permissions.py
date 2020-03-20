d407135fb86e37d15b27db71b0a205dd81a2d992
smoquet <you@example.com>
2020-03-20 12:59:26 +0100

    removes unused code

--- apps/institution/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class UserHasInstitutionScope(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user.has_perm('badgeuser.has_institution_scope'):
-            return True
-        return False
