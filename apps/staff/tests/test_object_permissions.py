00065dbb0f643ac674d06b1a653c4c203c085d31
smoquet <you@example.com>
2020-04-01 17:54:51 +0200

    adds update staff membership test

--- apps/staff/tests/test_object_permissions.py
+++ apps/staff/tests/test_object_permissions.py
@@ -20,7 +20,7 @@ class ObjectPermissionTests(BadgrTestCase):
             "user": teacher2.entity_id,
             "faculty": faculty.entity_id
         })
-        response = self.client.post('/staff/faculty/{}/create'.format(faculty.entity_id),
+        response = self.client.post('/staff-membership/faculty/{}/create'.format(faculty.entity_id),
                                     data, content_type='application/json')
         self.assertEqual(404, response.status_code)
 
@@ -40,7 +40,7 @@ class ObjectPermissionTests(BadgrTestCase):
             "user": teacher2.entity_id,
             "faculty": faculty.entity_id
         })
-        response = self.client.post('/staff/faculty/{}/create'.format(faculty.entity_id),
+        response = self.client.post('/staff-membership/faculty/{}/create'.format(faculty.entity_id),
                                     data, content_type='application/json')
         self.assertEqual(404, response.status_code)
 
@@ -61,7 +61,7 @@ class ObjectPermissionTests(BadgrTestCase):
             "user": teacher2.entity_id,
             "issuer": issuer.entity_id
         })
-        response = self.client.post('/staff/issuer/{}/create'.format(issuer.entity_id),
+        response = self.client.post('/staff-membership/issuer/{}/create'.format(issuer.entity_id),
                                     data, content_type='application/json')
         # TODO - expected 201
         self.assertEqual(response.status_code, 404)
@@ -69,10 +69,27 @@ class ObjectPermissionTests(BadgrTestCase):
     def test_permission_tree_cleanup_after_put(self):
         pass
 
-    def test_may_not_update_user(self):
-        pass
-
     def test_update_staff_membership(self):
+        teacher1 = self.setup_teacher(authenticate=True)
+        teacher2 = self.setup_teacher(institution=teacher1.institution)
+        faculty = self.setup_faculty(institution=teacher1.institution)
+        self.setup_staff_membership(teacher1, faculty, may_read=True, may_administrate_users=True)
+        staff = self.setup_staff_membership(teacher2, faculty, may_read=True, may_administrate_users=False)
+        data = json.dumps({
+            "may_create": 0,
+            "may_read": 1,
+            "may_update": 0,
+            "may_delete": 0,
+            "may_sign": 0,
+            "may_award": 0,
+            "may_administrate_users": 1,
+        })
+        response = self.client.put('/staff-membership/faculty/change/{}'.format(staff.entity_id),
+                                    data, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_may_not_update_user(self):
         pass
 
     def test_may_not_change_permissions_you_dont_have_yourself(self):
