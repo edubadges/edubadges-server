bd9c1f5245db8db0b91928e51ed910de89a84856
smoquet <you@example.com>
2020-04-01 18:11:45 +0200

    adds to tests

--- apps/staff/tests/test_object_permissions.py
+++ apps/staff/tests/test_object_permissions.py
@@ -93,22 +93,26 @@ class ObjectPermissionTests(BadgrTestCase):
         teacher2 = self.setup_teacher()
         faculty = self.setup_faculty(institution=teacher2.institution)
         self.setup_staff_membership(teacher1, faculty, may_read=True, may_administrate_users=True)
-        staff = self.setup_staff_membership(teacher2, faculty, may_read=True)
-        data = json.dumps({
+        staff = self.setup_staff_membership(teacher2, faculty, may_read=True, may_sign=True)
+        data = {
             "may_create": 0,
             "may_read": 1,
-            "may_update": 1,
+            "may_update": 0,
             "may_delete": 0,
             "may_sign": 0,
             "may_award": 0,
             "may_administrate_users": 1,
-        })
+        }
         response = self.client.put('/staff-membership/faculty/change/{}'.format(staff.entity_id),
-                                   data, content_type='application/json')
+                                   json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 404)
+        data["may_sign"] = 1
+        data["may_create"] = 0
 
     def test_may_not_assign_permissions_you_dont_have_yourself(self):
-        pass
+        response = self.client.put('/staff-membership/faculty/change/{}'.format(staff.entity_id),
+                                   json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 404)
 
     def test_may_not_remove_institution_staff_membership(self):
         pass
