e330f974c4a6d2287f9b82b4cd594f0bb8c58b87
smoquet <smoquet@users.noreply.github.com>
2020-04-14 18:15:27 +0200

    all slugs now entity_id in urls

--- apps/issuer/api_urls.py
+++ apps/issuer/api_urls.py
@@ -6,12 +6,12 @@ from issuer.api import (IssuerDetail, BadgeClassDetail, BadgeInstanceDetail, Bat
 
 urlpatterns = [
     url(r'^create$', IssuerList.as_view(), name='issuer_list'),
-    url(r'^edit/(?P<slug>[^/]+)$', IssuerDetail.as_view(), name='issuer_detail'),
+    url(r'^edit/(?P<entity_id>[^/]+)$', IssuerDetail.as_view(), name='issuer_detail'),
     url(r'^badgeclasses/create$', BadgeClassList.as_view(), name='badgeclass_list'),
-    url(r'^badgeclasses/edit/(?P<slug>[^/]+)$', BadgeClassDetail.as_view(), name='badgeclass_detail'),
+    url(r'^badgeclasses/edit/(?P<entity_id>[^/]+)$', BadgeClassDetail.as_view(), name='badgeclass_detail'),
 
-    url(r'^badgeclasses/award-enrollments/(?P<slug>[^/]+)$', BatchAwardEnrollments.as_view(), name='badgeclass_award_enrollments'),
-    url(r'^issuers/(?P<issuerSlug>[^/]+)/badges/(?P<badgeSlug>[^/]+)/assertions/(?P<slug>[^/]+)$', BadgeInstanceDetail.as_view(), name='badgeinstance_detail'),
+    url(r'^badgeclasses/award-enrollments/(?P<entity_id>[^/]+)$', BatchAwardEnrollments.as_view(), name='badgeclass_award_enrollments'),
+    url(r'^issuers/(?P<issuerSlug>[^/]+)/badges/(?P<badgeSlug>[^/]+)/assertions/(?P<entity_id>[^/]+)$', BadgeInstanceDetail.as_view(), name='badgeinstance_detail'),
     url(r'^batchSign$', BatchSignAssertions.as_view(), name='batch_sign'),
     url(r'^timestamped-assertions$', TimestampedBadgeInstanceList.as_view(), name='timestamped_badgeinstances'),
 ]
