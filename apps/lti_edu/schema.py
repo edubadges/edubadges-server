4465ff4c67be0b31b916c268d844549d213b3e1f
Okke Harsta <oharsta@zilverline.com>
2020-04-23 14:45:51 +0200

    Retrieve enrollments for user

--- apps/lti_edu/schema.py
+++ apps/lti_edu/schema.py
@@ -1,10 +1,10 @@
 import graphene
 from graphene_django.types import DjangoObjectType
+
 from .models import StudentsEnrolled
 
 
 class StudentsEnrolledType(DjangoObjectType):
-
     class Meta:
         model = StudentsEnrolled
         fields = ('entity_id', 'badge_class', 'date_created', 'date_consent_given',
@@ -12,12 +12,7 @@ class StudentsEnrolledType(DjangoObjectType):
 
 
 class Query(object):
-
     enrollments = graphene.List(StudentsEnrolledType)
 
     def resolve_enrollments(self, info, **kwargs):
-        badge_classes = info.context.user.get_all_badgeclasses_with_permissions(['may_award'])
-        enrollments = []
-        for bc in badge_classes:
-            enrollments += bc.cached_enrollments()
-        return enrollments
+        return StudentsEnrolled.objects.filter(user=info.context.user)
