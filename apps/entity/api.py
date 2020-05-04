943018c2015d9d8305121567541914d1a0698715
Okke Harsta <oharsta@zilverline.com>
2020-05-04 15:28:53 +0200

    If an object is owned by an user, then it is accessible for this user

--- apps/entity/api.py
+++ apps/entity/api.py
@@ -84,7 +84,17 @@ class BaseEntityListView(BaseEntityView):
 
 class VersionedObjectMixin(object):
 
+    @staticmethod
+    def _is_owned(request, obj):
+        user = getattr(request, "user", None)
+        user_id = user.id if user else None
+        user_id_object = getattr(obj, "user_id", None)
+        return user_id and user_id_object and user_id == user_id_object
+
     def has_object_permissions(self, request, obj):
+        if self._is_owned(request, obj):
+            return True
+
         for permission in self.get_permissions():
             if not permission.has_object_permission(request, self, obj):
                 return False
@@ -99,7 +109,7 @@ class VersionedObjectMixin(object):
         except self.model.DoesNotExist:
             pass
         else:
-            if not self.has_object_permissions(request, self.object):
+            if not self._is_owned(request, self.object) and not self.has_object_permissions(request, self.object):
                 raise Http404
             return self.object
 
