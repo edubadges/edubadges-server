637bf219f4a8fbd1f0c1a87f034c9ce2e83bb44e
smoquet <smoquet@users.noreply.github.com>
2020-05-05 12:27:27 +0200

    adds institution tests

--- apps/institution/tests/test_institution.py
+++ apps/institution/tests/test_institution.py
@@ -1,14 +1,29 @@
+import json
 from mainsite.tests import BadgrTestCase
+from institution.testfiles.helper import faculty_json, institution_json
 
 
-class InsititutionTest(BadgrTestCase):
+class InstitutionTest(BadgrTestCase):
 
     def test_create_faculty(self):
-        pass
+        teacher1 = self.setup_teacher(authenticate=True)
+        self.setup_staff_membership(teacher1, teacher1.institution, may_create=True)
+        response = self.client.post("/institution/faculties/create", data=json.dumps(faculty_json),
+                                      content_type='application/json')
+        self.assertEqual(response.status_code, 201)
 
     def test_edit_institution(self):
-        "may not delete, or create, just edit"
-        pass
+        teacher1 = self.setup_teacher(authenticate=True)
+        self.setup_staff_membership(teacher1, teacher1.institution, may_read=True, may_create=True, may_update=True)
+        institution_json['name'] = 'NewName'
+        response = self.client.put("/institution/edit/{}".format(teacher1.institution.entity_id),
+                                    data=json.dumps(institution_json), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(teacher1.institution.name, 'NewName')
+        response = self.client.delete("/institution/edit/".format(teacher1.institution.entity_id),
+                                      content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+
 
     def test_institution_schema(self):
         pass
