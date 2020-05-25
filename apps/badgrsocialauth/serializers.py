5b47bfa07822ef8ee3f9eeda00f85c72ae62c119
Okke Harsta <oharsta@zilverline.com>
2020-05-25 16:17:07 +0200

    Get the eduid from the extra_data

--- apps/badgrsocialauth/serializers.py
+++ apps/badgrsocialauth/serializers.py
@@ -10,17 +10,19 @@ class BadgrSocialAccountSerializerV1(serializers.Serializer):
     def to_representation(self, instance):
         representation = super(BadgrSocialAccountSerializerV1, self).to_representation(instance)
         provider = instance.get_provider()
-        common_fields = provider.extract_common_fields(instance.extra_data)
+        extra_data = instance.extra_data
+        common_fields = provider.extract_common_fields(extra_data)
         email = common_fields.get('email', None)
-        if not email and 'userPrincipalName' in instance.extra_data:
-            email = instance.extra_data['userPrincipalName']
-        entitlements = instance.extra_data.get('eduperson_entitlement', [])
+        if not email and 'userPrincipalName' in extra_data:
+            email = extra_data['userPrincipalName']
+        entitlements = extra_data.get('eduperson_entitlement', [])
         verified_by_institution = "urn:mace:eduid.nl:entitlement:verified-by-institution" in entitlements
         representation.update({
             'firstName': common_fields.get('first_name', None),
             'lastName': common_fields.get('last_name', None),
             'primaryEmail': email,
-            'verified_by_institution': verified_by_institution
+            'verified_by_institution': verified_by_institution,
+            'eduid': extra_data.get('eduid', None)
         })
 
         return representation
