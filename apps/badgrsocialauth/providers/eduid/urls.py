a16569d20df04af6014885e2b30f2c60548dc272
Okke Harsta <oharsta@zilverline.com>
2020-07-22 14:03:10 +0200

    Typo

--- apps/badgrsocialauth/providers/eduid/urls.py
+++ apps/badgrsocialauth/providers/eduid/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    # note: in registering the badgr app at SurfConext, the callback url is explicitely registered
+    # note: in registering the badgr app at SurfConext, the callback url is explicitly registered
     url('^eduid/login/callback/$', views.callback, name='edu_id_callback'),
     url('^eduid/login/$', views.login, name="edu_id_login"),
     url('^eduid/login/terms_accepted/(?P<state>[^/]+)/(?P<id_token>[^/]+)', views.after_terms_agreement,
