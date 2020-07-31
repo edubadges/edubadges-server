144092d311ae10ff73b0c9061b624ad749c51db5
smoquet <smoquet@users.noreply.github.com>
2020-07-31 15:58:30 +0200

    fixes migration order

--- apps/institution/migrations/0025_auto_20200723_0954.py
+++ apps/institution/migrations/0025_auto_20200723_0954.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('institution', '0025_institution_grondslag'),
+        ('institution', '0024_auto_20200615_1359'),
     ]
 
     operations = [
