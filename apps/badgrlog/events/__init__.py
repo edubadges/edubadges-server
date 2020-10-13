cc426d15364e476b7650d444f07a42de52cfd2d5
smoquet <smoquet@users.noreply.github.com>
2020-10-13 13:34:24 +0200

    removes obsolete logging classes

--- apps/badgrlog/events/__init__.py
+++ apps/badgrlog/events/__init__.py
@@ -1,8 +1,6 @@
 # Created by wiggins@concentricsky.com on 8/27/15.
 
-from .badgebook import *
 from .badgeuser import *
-from .composition import *
 from .issuer import *
 from .public import *
 from .staff import *
