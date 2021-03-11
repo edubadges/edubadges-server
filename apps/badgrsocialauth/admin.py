373245d892608b47d859800e3b6e1b3dcd7a211d
smoquet <smoquet@users.noreply.github.com>
2021-03-11 09:53:45 +0100

    adapts socialaccount admin detailpage

--- apps/badgrsocialauth/admin.py
+++ apps/badgrsocialauth/admin.py
@@ -4,6 +4,7 @@ from allauth.socialaccount.admin import SocialAccountAdmin
 class BadgrSocialAccountAdmin(SocialAccountAdmin):
     list_display = ('user', '_eduid', 'provider', 'date_joined')
     readonly_fields = ('_eduid', )
+    exclude = ('uid',)
 
     def _eduid(self, obj):
         return obj.uid
