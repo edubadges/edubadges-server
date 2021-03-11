f5162dea65683d0d8393699189b4cb61a3b686b4
Okke Harsta <oharsta@zilverline.com>
2021-03-11 14:23:08 +0100

    Formatting

--- apps/institution/schema.py
+++ apps/institution/schema.py
@@ -40,9 +40,8 @@ class InstitutionType(UserProvisionmentResolverMixin, PermissionsResolverMixin, 
     class Meta:
         model = Institution
         fields = ('entity_id', 'identifier', 'name_english', 'name_dutch', 'staff', 'created_at', 'description_english',
-                  'description_dutch', 'institution_type',
-                  'image_english', 'image_dutch', 'grading_table', 'brin', 'content_type_id', 'grondslag_formeel',
-                  'grondslag_informeel', 'default_language')
+                  'description_dutch', 'institution_type', 'image_english', 'image_dutch', 'grading_table', 'brin',
+                  'content_type_id', 'grondslag_formeel', 'grondslag_informeel', 'default_language')
 
     faculties = graphene.List(FacultyType)
     public_faculties = graphene.List(FacultyType)
