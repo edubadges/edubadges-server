9e5bbe07872417f2e760de98c29dd34cea953587
Okke Harsta <oharsta@zilverline.com>
2021-09-23 11:46:14 +0200

    Added unbake tests

--- apps/tests/test_unbaking.py
+++ apps/tests/test_unbaking.py
@@ -1,3 +1,5 @@
+import hashlib
+import json
 import os
 import unittest
 
@@ -15,5 +17,18 @@ class TestBaking(unittest.TestCase):
     def test_unbake_png(self):
         dirname = os.path.dirname(__file__)
         with open(os.path.join(dirname, 'files', 'introduction_to_political_science_edubadge.png'), 'rb') as image:
-            verify_url = unbake(image)
-            self.assertEqual('http://localhost:8000/public/assertions/0SiqaDWfS3CXi2gCg72j_A', verify_url)
+            assertion = unbake(image)
+            data = json.loads(assertion)
+            self.assertEqual('http://localhost:8000/public/assertions/DeUa447WR4mScC-4DgJomw', data['id'])
+
+    def test_unbake_eu_badgr_png(self):
+        dirname = os.path.dirname(__file__)
+        with open(os.path.join(dirname, 'files', 'test_badge_interoperability.png'), 'rb') as image:
+            assertion = unbake(image)
+            data = json.loads(assertion)
+            self.assertEqual('https://api.eu.badgr.io/public/assertions/10Ou1MgvRRCEXvZufypxeA', data['id'])
+
+            recipient = data['recipient']
+            salt = recipient['salt']
+            value = 'sha256$' + hashlib.sha256('oharsta@zilverline.com'.encode() + salt.encode()).hexdigest()
+            self.assertEqual(value, recipient['identity'])
