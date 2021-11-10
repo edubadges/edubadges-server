16474ff2defbfb7500beacc8b1b6d65901cbf3da
Okke Harsta <oharsta@zilverline.com>
2021-11-10 15:32:27 +0100

    Ability to login with LTI1.3

--- apps/lti13/views.py
+++ apps/lti13/views.py
@@ -1,67 +1,40 @@
-import datetime
 import os
-import urllib.error
 import urllib.parse
-import urllib.parse
-import urllib.request
+from urllib.parse import parse_qs
+from urllib.parse import urlparse
 
-from allauth.account.adapter import get_adapter
 from allauth.account.utils import perform_login
 from allauth.socialaccount import app_settings
 from allauth.socialaccount.models import SocialLogin
-from allauth.socialaccount.providers.base import AuthProcess
-from django.contrib.auth import authenticate
-from django.contrib.auth import logout
-from django.core.exceptions import ValidationError
-from django.http import HttpResponseBadRequest, HttpResponseForbidden
-from django.urls import reverse, NoReverseMatch
-from django.views.generic import RedirectView
-from rest_framework import status
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from badgeuser.models import BadgeUser
-from badgrsocialauth.permissions import IsSuperUser
-from badgrsocialauth.utils import set_url_query_params, set_session_badgr_app, get_session_badgr_app, \
-    get_session_verification_email, set_session_authcode
-from institution.models import Institution
-from mainsite.models import BadgrApp
-
-from allauth.socialaccount.models import SocialLogin
 from django.conf import settings
-from django.http import HttpResponseForbidden, JsonResponse
+from django.core.cache import caches  # type: ignore
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.views.decorators.http import require_POST
-from pylti1p3.contrib.django import DjangoOIDCLogin, DjangoMessageLaunch, DjangoCacheDataStorage
-from pylti1p3.grade import Grade
-from pylti1p3.tool_config.abstract import ToolConfAbstract
-from pylti1p3.lineitem import LineItem
+from pylti1p3.contrib.django import DjangoOIDCLogin, DjangoMessageLaunch
+from pylti1p3.launch_data_storage.cache import CacheDataStorage
 from pylti1p3.tool_config import ToolConfJsonFile
-from badgrsocialauth.utils import set_url_query_params, set_session_badgr_app, get_session_badgr_app, \
-    get_session_verification_email, set_session_authcode
-from mainsite.models import BadgrApp
 
 from badgeuser.models import BadgeUser
+from badgrsocialauth.utils import set_session_badgr_app
+from institution.models import Institution
 from mainsite import TOP_DIR
 from mainsite.models import BadgrApp
 
 
-class ExtendedDjangoMessageLaunch(DjangoMessageLaunch):
+class DjangoNoSessionCacheDataStorage(CacheDataStorage):
+    _cache = None
 
-    def validate_nonce(self):
-        """
-        Probably it is bug on "https://lti-ri.imsglobal.org":
-        site passes invalid "nonce" value during deep links launch.
-        Because of this in case of iss == http://imsglobal.org just skip nonce validation.
-        """
-        iss = self.get_iss()
-        deep_link_launch = self.is_deep_link_launch()
-        if iss == "http://imsglobal.org" and deep_link_launch:
-            return self
-        return super(ExtendedDjangoMessageLaunch, self).validate_nonce()
+    def __init__(self, cache_name='default', **kwargs):
+        self._cache = caches[cache_name]
+        super(DjangoNoSessionCacheDataStorage, self).__init__(cache_name, **kwargs)
 
-# TODO - Change this with Database tool config
+    def get_session_cookie_name(self):
+        # We are stateless as the client and server do not share the domain
+        return None
+
+
+# TODO - Change this with DJango Database tool config
 def get_tool_conf():
     lti_config_path = os.path.join(TOP_DIR, 'apps', 'lti13', 'config', 'lms.json')
     tool_conf = ToolConfJsonFile(lti_config_path)
@@ -69,7 +42,7 @@ def get_tool_conf():
 
 
 def get_launch_data_storage():
-    return DjangoCacheDataStorage()
+    return DjangoNoSessionCacheDataStorage()
 
 
 def get_launch_url(request):
@@ -93,51 +66,44 @@ def login(request):
 def launch(request):
     tool_conf = get_tool_conf()
     launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedDjangoMessageLaunch(request, tool_conf, launch_data_storage=launch_data_storage)
+    message_launch = DjangoMessageLaunch(request, tool_conf, launch_data_storage=launch_data_storage)
     message_launch_data = message_launch.get_launch_data()
-
-    if not message_launch.has_nrps():
-        return HttpResponseForbidden("Don't have names and roles!")
-
-    if not message_launch.has_ags():
-        return HttpResponseForbidden("Don't have grades!")
-
-    ags = message_launch.get_ags()
-    nrps = message_launch.get_nrps()
-
-    members = nrps.get_members()
-    line_items = ags.get_lineitems()
-    grades = [ags.get_grades(line_item) for line_item in line_items]
+    # Get the mandatory data from the launch data
     email = message_launch_data['email']
+    issuer = message_launch_data['iss']
+    client_id = message_launch_data['aud']
+    # This can not fail as the launch would have been aborted
+    registration = tool_conf.find_registration_by_params(issuer, client_id)
+    institution = Institution.objects.get(identifier=registration.get_institution_identifier())
+    launch_id = message_launch.get_launch_id()
+    if not institution:
+        return redirect(f"{settings.UI_URL}/lti?status=invalid_institution")
+
+    user = request.user
+    if not user.is_anonymous and isinstance(user, BadgeUser) and user.is_teacher:
+        return redirect(f"{settings.UI_URL}/lti?status=user_logged_in&launch_id={launch_id}")
+
     try:
         user = BadgeUser.objects.get(email=email, is_teacher=True)
     except BadgeUser.DoesNotExist:
-        request.session['lti_context'] = {
-            'email': message_launch_data['email'],
-            'known_user': False
-        }
-        return redirect(f"{settings.UI_URL}/lti")
+        args = {"status": "unknown_user"}
+        admins = list(filter(lambda u: u.may_administrate_users, institution.staff))
+        if len(admins) > 0:
+            args["admin_email"] = admins[0].user.email
+        return redirect(f"{settings.UI_URL}/lti?{urllib.parse.urlencode(args)}")
 
     social_account = user.get_social_account()
-    sociallogin = SocialLogin(account=social_account, email_addresses=[email for email in user.email_items])
-    sociallogin.user = user
-    badgr_app = BadgrApp.objects.filter(pk=user.badgrapp_id).first()
-    if not badgr_app:
-        badgr_app = BadgrApp.objects.all().first()
+    social_login = SocialLogin(account=social_account, email_addresses=[email for email in user.email_items])
+    social_login.user = user
+    badgr_app = BadgrApp.objects.all().first()
     set_session_badgr_app(request, badgr_app)
-    ret = perform_login(request, sociallogin.user,
+    ret = perform_login(request, social_login.user,
                         email_verification=app_settings.EMAIL_VERIFICATION,
-                        redirect_url=sociallogin.get_redirect_url(request),
-                        signal_kwargs={"sociallogin": sociallogin})
-    return Response({"url": ret.url}, status=status.HTTP_200_OK)
-
-    request.session['lti_context'] = {
-        'email': message_launch_data['email'],
-        'known_user': True,
-        'launch_id': message_launch.get_launch_id(),
-        'url':
-    }
-    return redirect(f"{settings.UI_URL}/lti")
+                        redirect_url=social_login.get_redirect_url(request),
+                        signal_kwargs={"sociallogin": social_login})
+    auth_token = parse_qs(urlparse(ret.url).query)['authToken'][0]
+    args = {"status": "known_user", "launch_id": launch_id, "auth_token": auth_token}
+    return redirect(f"{settings.UI_URL}/lti?{urllib.parse.urlencode(args)}")
 
 
 def get_jwks(request):
@@ -145,23 +111,25 @@ def get_jwks(request):
     return JsonResponse(tool_conf.get_jwks(), safe=False)
 
 
-def get_lti_context(request):
-    return JsonResponse(request.session.get('lti_context'), safe=False)
+def get_lti_context(request, launch_id):
+    tool_conf = get_tool_conf()
+    launch_data_storage = get_launch_data_storage()
+    message_launch = ExtendedDjangoMessageLaunch.from_cache(launch_id, request, tool_conf,
+                                                            launch_data_storage=launch_data_storage)
+    launch_data = message_launch.get_launch_data()
+    return JsonResponse(launch_data, safe=False)
 
-def render_auth_error(issuer, tool_conf: ToolConfAbstract):
-    tool_conf.get_iss_config('issuer')
-    extra_context = {}
-    institution = Institution.objects.get(identifier=institution_identifier)
-    Institution
-    if institution and institution.cached_staff():
-        cached_staff = institution.cached_staff()
-        admins = list(filter(lambda u: u.may_administrate_users, cached_staff))
-        if len(admins) > 0:
-            extra_context["admin_email"] = admins[0].user.email
 
-    error = 'Sorry, you can not register without an invite.'
-    extra_context["code"] = AuthErrorCode.REGISTER_WITHOUT_INVITE
-    if request.user.date_joined.today().date() == datetime.datetime.today().date():  # extra protection before deletion
-        request.user.delete()
-    return render_authentication_error(request, SurfConextProvider.id, error,
-                                       extra_context=extra_context)
+def get_grades(request):
+    tool_conf = get_tool_conf()
+    launch_data_storage = get_launch_data_storage()
+    lti_context = request.session.get('lti_context')
+    message_launch = ExtendedDjangoMessageLaunch.from_cache(lti_context.get('launch_id'), request, tool_conf,
+                                                            launch_data_storage=launch_data_storage)
+    ags = message_launch.get_ags()
+    nrps = message_launch.get_nrps()
+
+    # members = nrps.get_members()
+    line_items = ags.get_lineitems()
+    grades = [ags.get_grades(line_item) for line_item in line_items]
+    return JsonResponse(grades, safe=False)
