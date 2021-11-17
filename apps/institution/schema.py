1c6883c966819066aa688aa6c292ba0916d50bfc
Okke Harsta <oharsta@zilverline.com>
2021-11-17 13:18:53 +0100

    Do not return archived issuers

--- apps/institution/schema.py
+++ apps/institution/schema.py
@@ -99,7 +99,7 @@ class Query(object):
 
     def resolve_issuers(self, info, **kwargs):
         user = info.context.user
-        return [iss for iss in Issuer.objects.filter(faculty__institution=user.institution) if iss.has_permissions(user, ['may_update'])]
+        return [iss for iss in Issuer.objects.filter(faculty__institution=user.institution, archived=False) if iss.has_permissions(user, ['may_update'])]
 
     def resolve_institutions(self, info, **kwargs):
         return [inst for inst in Institution.objects.all() if inst.has_permissions(info.context.user, ['may_read'])]
