f7fa4d17d8878f70935dd1728d566969cda77e9f
Okke Harsta <oharsta@zilverline.com>
2022-11-17 11:38:00 +0100

    Superuser interface backend changes

--- apps/directaward/admin.py
+++ apps/directaward/admin.py
@@ -7,13 +7,32 @@ from .models import DirectAward, DirectAwardBundle
 
 
 class DirectAwardAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recipient_email', 'eppn', admin_list_linkify('badgeclass', 'name'), 'status')
+    list_display = ('id', 'recipient_email', 'eppn', 'created_at', admin_list_linkify('badgeclass', 'name'),
+                    'institution_identifier', 'status')
+    list_display_links = ('recipient_email', 'eppn')
+    list_filter = ('created_at', 'badgeclass__name', 'badgeclass__issuer__faculty__institution__identifier')
+    search_fields = ('recipient_email', 'eppn', 'badgeclass__name',)
+
+    def institution_identifier(self, obj):
+        return obj.badgeclass.issuer.faculty.institution.identifier
+
+    institution_identifier.admin_order_field = 'badgeclass__issuer__faculty__institution__identifier'
+    institution_identifier.short_description = 'Institution'
 
 
 class DirectAwardBundleAdmin(admin.ModelAdmin):
     list_display = (
-    'assertion_count', 'direct_award_count', 'direct_award_rejected_count', 'direct_award_revoked_count',
-    admin_list_linkify('badgeclass', 'name'))
+        'assertion_count', 'direct_award_count', 'direct_award_rejected_count', 'direct_award_revoked_count',
+        'created_at', admin_list_linkify('badgeclass', 'name'), 'institution_identifier')
+
+    list_filter = ('created_at', 'badgeclass__issuer__faculty__institution__identifier')
+    search_fields = ('badgeclass__name',)
+
+    def institution_identifier(self, obj):
+        return obj.badgeclass.issuer.faculty.institution.identifier
+
+    institution_identifier.admin_order_field = 'badgeclass__issuer__faculty__institution__identifier'
+    institution_identifier.short_description = 'Institution'
 
 
 badgr_admin.register(DirectAward, DirectAwardAdmin)
