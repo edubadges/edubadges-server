968e9b342d537a1db7c030b0667041ea3ac5cbb9
Okke Harsta <oharsta@zilverline.com>
2023-08-14 09:49:05 +0200

    Exclude create Direct Award from CSRF filter

--- apps/directaward/api_urls.py
+++ apps/directaward/api_urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-
+from django.views.decorators.csrf import csrf_exempt
 from directaward.api import (DirectAwardBundleList, DirectAwardDetail, DirectAwardAccept, DirectAwardRevoke, DirectAwardResend)
 
 urlpatterns = [
-    url(r'^create$', DirectAwardBundleList.as_view(), name='direct_award_bundle_list'),
+    url(r'^create$', csrf_exempt(DirectAwardBundleList.as_view()), name='direct_award_bundle_list'),
     url(r'^edit/(?P<entity_id>[^/]+)$', DirectAwardDetail.as_view(), name='direct_award_detail'),
     url(r'^accept/(?P<entity_id>[^/]+)$', DirectAwardAccept.as_view(), name='direct_award_accept'),
     url(r'^revoke-direct-awards$', DirectAwardRevoke.as_view(), name='direct_award_revoke'),
