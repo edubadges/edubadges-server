5d3baaf93321129a5cf616b257491e803321eb24
Okke Harsta <oharsta@zilverline.com>
2023-11-10 13:28:57 +0100

    More queries

--- queries/micro_credentials.sql
+++ queries/micro_credentials.sql
@@ -1,6 +1,6 @@
 select b.id, b.name as badgeclass_name, i.name_english, f.name_english as faculty_name, ins.name_english as institution_name, ins.identifier, b.created_at ,
 (select original_json from issuer_badgeclassextension where name = 'extensions:EQFExtension' and badgeclass_id = b.id limit 1) as eqf_value,
-(select original_json from issuer_badgeclassextension where name = 'extensions:StudyLoadExtension' and badgeclass_id = b.id limit 1) as study_load
+(select original_json from issuer_badgeclassextension where name = 'extensions:ECTSExtension' and badgeclass_id = b.id limit 1) as study_load
  from issuer_badgeclass b
  inner join issuer_issuer i on i.id = b.issuer_id
  inner join institution_faculty f on f.id = i.faculty_id
---
+++ queries/various.sql
@@ -0,0 +1,25 @@
+select b.id, b.name as badgeclass_name,  ins.name_english as institution_name, ins.identifier
+ from issuer_badgeclass b
+ inner join issuer_issuer i on i.id = b.issuer_id
+ inner join institution_faculty f on f.id = i.faculty_id
+ inner join institution_institution ins on ins.id = f.institution_id
+ where b.is_micro_credentials = 1 and ins.institution_type = 'MBO';
+
+ select ins.institution_type
+ from issuer_badgeclass b
+ inner join issuer_badgeclassextension ext on ext.badgeclass_id = b.id
+ inner join issuer_issuer i on i.id = b.issuer_id
+ inner join institution_faculty f on f.id = i.faculty_id
+ inner join institution_institution ins on ins.id = f.institution_id
+ where b.is_micro_credentials = 1 and ins.institution_type is not null;
+
+
+
+ select b.id, b.name as badgeclass_name, ins.name_english as institution_name, ins.identifier, b.created_at ,
+(select original_json from issuer_badgeclassextension where name = 'extensions:EQFExtension' and badgeclass_id = b.id limit 1) as eqf_value,
+(select original_json from issuer_badgeclassextension where name = 'extensions:ECTSExtension' and badgeclass_id = b.id limit 1) as study_load
+ from issuer_badgeclass b
+ inner join issuer_issuer i on i.id = b.issuer_id
+ inner join institution_faculty f on f.id = i.faculty_id
+ inner join institution_institution ins on ins.id = f.institution_id
+ where b.is_micro_credentials = 1 and ins.institution_type is not null;
