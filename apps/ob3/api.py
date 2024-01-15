d143c95d2ecff10f6779c520bc03eceefd104e30
Okke Harsta <oharsta@zilverline.com>
2024-01-15 11:45:09 +0100

    OB3 endpoit

--- apps/ob3/api.py
+++ apps/ob3/api.py
@@ -1,14 +1,21 @@
+import base64
+import uuid
+from io import BytesIO
+
+import qrcode
+import requests
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from issuer.models import BadgeInstance
-from mainsite.permissions import TeachPermission
+from mainsite.settings import OB3_API_URL, UI_URL
 
 
 class CredentialsView(APIView):
-    permission_classes = (TeachPermission,)
+    permission_classes = (permissions.AllowAny,)
+    http_method_names = ['post']
 
     def post(self, request, **kwargs):
         badge_id = request.data.get('badge_id')
@@ -16,17 +23,43 @@ class CredentialsView(APIView):
         if badge_instance.user != request.user:
             raise Http404
 
-        return Response(res, status=status.HTTP_200_OK)
+        badgeclass = badge_instance.badgeclass
+        criteria = badgeclass.criteria_url if badgeclass.criteria_url else badgeclass.criteria_text
+        subject_id = str(uuid.uuid4())
+        request_data = {
+            "subjectId": subject_id,
+            "credential": {
+                "credentialSubject": {
+                    "id": "",
+                    "type": [
+                        "AchievementSubject"
+                    ],
+                    "achievement": {
+                        "id": f"{UI_URL}/public/assertions/{badge_instance.entity_id}",
+                        "type": [
+                            "Achievement"
+                        ],
+                        "criteria": {
+                            "narrative": criteria
+                        },
+                        "description": badgeclass.description,
+                        "name": badgeclass.name
+                    }
+                }
+            }
+        }
+        requests.post(json=request_data,
+                      url=f"{OB3_API_URL}/v1/credentials",
+                      headers={'Accept': 'application/json'})
+        subject_id = {"subjectId": subject_id}
+        response = requests.post(json=subject_id,
+                                 url=f"{OB3_API_URL}/v1/offers",
+                                 headers={'Accept': 'application/json'})
+        response_json = response.json
 
+        img = qrcode.make(response_json)
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
 
-class OffersView(APIView):
-    permission_classes = (TeachPermission,)
-
-    def post(self, request, **kwargs):
-        badge_id = request.data.get('badge_id')
-        badge_instance = BadgeInstance.objects.get(id=badge_id)
-        if badge_instance.user != request.user:
-            raise Http404
-        # POST to ob3 endpoint and return the results
-
-        return Response(res, status=status.HTTP_200_OK)
+        return Response({"qr_code_base64": img_str}, status=status.HTTP_201_CREATED)
--- apps/ob3/api_urls.py
+++ apps/ob3/api_urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 from django_api_proxy.views import ProxyView
 
-from ob3.api import CredentialsView, OffersView
+from ob3.api import CredentialsView
 
 urlpatterns = [
-    url(r'^v1/credentials', CredentialsView.as_view(), name='credentials'),
-    url(r'^v1/offers', OffersView.as_view(), name='credentials'),
+    url(r'^v1/ob3', CredentialsView.as_view(), name='credentials'),
     url(r'^.well-known/oauth-authorization-server',
         ProxyView.as_view(source='.well-known/oauth-authorization-server'), name='oauth-authorization-server'),
     url(r'^.well-known/openid-credential-issuer',
