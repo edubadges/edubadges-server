da04f6b5d4b4e0c141ba155ef549595e43230e83
Bèr Kessels <ber@berk.es>
2024-09-25 18:01:19 +0200

    fix: Allow migrations to create indexes that are guaranteed to be under 3072

--- apps/issuer/migrations/0044_auto_20180713_0658.py
+++ apps/issuer/migrations/0044_auto_20180713_0658.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='badgeinstance',
             name='recipient_identifier',
-            field=models.EmailField(db_index=True, max_length=768),
+            field=models.EmailField(db_index=True, max_length=512),
         ),
     ]
