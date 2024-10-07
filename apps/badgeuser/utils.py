014c92a630856182b1114d1ec073d2e7ad7b8584
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2024-10-07 10:18:34 +0200

    Updated to sha256 hash when generating badgr username

--- apps/badgeuser/utils.py
+++ apps/badgeuser/utils.py
@@ -1,11 +1,15 @@
 import random
 import string
 import base64
-from hashlib import md5
+from hashlib import sha256
 
 
 def generate_badgr_username(email):
-    # md5 hash the email and then encode as base64 to take up only 25 characters
-    salted_email = (email + ''.join(random.choice(string.ascii_lowercase) for i in range(64))).encode('utf-8')
-    hashed = str(base64.b64encode(md5(salted_email).hexdigest().encode('utf-8')), 'utf-8')
+    # sha256 hash the email and then encode as base64 to take up only 25 characters
+    salted_email = (
+        email + "".join(random.choice(string.ascii_lowercase) for i in range(64))
+    ).encode("utf-8")
+    hashed = str(
+        base64.b64encode(sha256(salted_email).hexdigest().encode("utf-8")), "utf-8"
+    )
     return "badgr{}".format(hashed[:25])
