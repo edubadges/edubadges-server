7b325f251184c12c69cf6267ec68e1a11ae698d1
Bèr Kessels <ber@berk.es>
2024-10-09 12:02:51 +0200

    refactor: Split Post handling method into utility methods

--- apps/ob3/api.py
+++ apps/ob3/api.py
@@ -18,16 +18,48 @@ class CredentialsView(APIView):
     http_method_names = ['post']
 
     def post(self, request, **kwargs):
+        offer_id = str(uuid.uuid4())
         badge_id = request.data.get('badge_id')
-        badge_instance = BadgeInstance.objects.get(id=badge_id)
-        if badge_instance.user != request.user:
+
+        badge_instance = self.__badge_instance(badge_id, request.user)
+        request_data = self.__request_data(offer_id, badge_instance)
+
+        self.__issue_badge(badge_instance)
+        credential_offer = self.__get_offer(offer_id)
+
+        img = qrcode.make(credential_offer)
+
+        buffered = BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode()
+
+        return Response({"qr_code_base64": img_str}, status=status.HTTP_201_CREATED)
+
+    def __badge_instance(self, badge_id, user):
+        try:
+            return BadgeInstance.objects.get(id=badge_id, user=user)
+        except BadgeInstance.DoesNotExist:
             raise Http404
 
+    def __issue_badge(self, badge_instance):
+        requests.post(json=request_data,
+                      url=f"{OB3_API_URL}/v0/credentials",
+                      headers={'Accept': 'application/json'})
+
+    def __get_offer(self, offer_id):
+        offer_id = {"offerId": offer_id}
+        response = requests.post(json=offer_id,
+                                 url=f"{OB3_API_URL}/v0/offers",
+                                 headers={'Accept': 'application/json'})
+
+        return response.text
+
+
+    def __request_data(self, offer_id, badge_instance):
         badgeclass = badge_instance.badgeclass
         criteria = badgeclass.criteria_text
-        offer_id = str(uuid.uuid4())
         issuer = badgeclass.issuer
-        request_data = {
+        return {
             "offerId": offer_id,
             "credentialConfigurationId": "openbadge_credential",
             "credential": {
@@ -60,20 +92,3 @@ class CredentialsView(APIView):
                 }
             }
         }
-        requests.post(json=request_data,
-                      url=f"{OB3_API_URL}/v0/credentials",
-                      headers={'Accept': 'application/json'})
-
-        offer_id = {"offerId": offer_id}
-        response = requests.post(json=offer_id,
-                                 url=f"{OB3_API_URL}/v0/offers",
-                                 headers={'Accept': 'application/json'})
-
-        credential_offer = response.text
-        img = qrcode.make(credential_offer)
-
-        buffered = BytesIO()
-        img.save(buffered, format="PNG")
-        img_str = base64.b64encode(buffered.getvalue()).decode()
-
-        return Response({"qr_code_base64": img_str}, status=status.HTTP_201_CREATED)
