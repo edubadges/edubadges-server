395b2493f59d1296c8f44634d20d614ce8b90a6b
Daniel <daniel.ostkamp@surf.nl>
2024-10-31 10:42:34 +0100

    Added graphql caching

--- apps/mainsite/graphql_view.py
+++ apps/mainsite/graphql_view.py
@@ -1,6 +1,6 @@
 import logging
-
 from graphene_django.views import GraphQLView
+from django.core.cache import cache
 
 logger = logging.getLogger('Badgr.Debug')
 
@@ -17,11 +17,21 @@ class DisableIntrospectionMiddleware(object):
 
 
 class ExtendedGraphQLView(GraphQLView):
-
     def execute_graphql_request(self, request, data, query, variables, operation_name, show_graphiql=False):
-        res = super().execute_graphql_request(request, data, query, variables, operation_name,
-                                              show_graphiql=show_graphiql)
-        if res.errors:
-            logger.exception(str(res.errors))
-            res.invalid = True
+        key = hash(repr(query))
+        cache_time = 3600  # time in seconds for cache to be valid, 1 hour
+        # if cache key in table, return cached result
+        res = cache.get(key)
+        if not res:
+            logger.debug("No cache hit!")
+            res = super().execute_graphql_request(
+                request, data, query, variables, operation_name, show_graphiql=show_graphiql
+            )
+            if res.errors:
+                logger.exception(str(res.errors))
+                res.invalid = True
+            else:
+                cache.set(key, res, cache_time)
+        else:
+            logger.debug("Cache hit!")
         return res
