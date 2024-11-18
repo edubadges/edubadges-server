c538c4a039fea63093d1e3989ca3b41d3788f40f
Bèr Kessels <ber@berk.es>
2024-11-18 11:31:16 +0100

    feat: Add optional validFrom and validUntil fields to credential
    These are sourced from issued_on and expires_at fields on
    badge-instance.
    They won't be in the payload if the fields are Null (or blank or none)
    as per the OB3 spec.

--- apps/ob3/models.py
+++ apps/ob3/models.py
@@ -1,17 +1,25 @@
 # A plain old Python object (POPO) that represents an educational credential
+
 class EduCredential:
     def __init__(self, offer_id, credential_configuration_id, badge_instance):
         self.offer_id = offer_id
         self.credential_configuration_id = credential_configuration_id
         self.credential = Credential(
             issuer=badge_instance.badgeclass.issuer,
+            valid_from = badge_instance.issued_on,
             credential_subject= { "achievement": Achievement.from_badge_instance(badge_instance) }
         )
 
+        if badge_instance.expires_at:
+            self.credential.valid_until = badge_instance.expires_at
+
 class Credential:
-    def __init__(self, issuer, credential_subject):
+    def __init__(self, issuer, valid_from, credential_subject, **kwargs):
         self.issuer = issuer
+        self.valid_from = valid_from
         self.credential_subject = credential_subject
+
+        self.valid_until = kwargs.get('valid_until', None)
 
 class Achievement:
     def __init__(self, id, criteria, description, name, image):
--- apps/ob3/tests.py
+++ apps/ob3/tests.py
@@ -1,5 +1,7 @@
 from django.test import SimpleTestCase
 
+from datetime import datetime as DateTime
+
 from .models import EduCredential
 from .serializers import EduCredentialSerializer
 
@@ -19,13 +21,15 @@ class BadgeInstanceMock:
     def __init__(self):
         self.entity_id = "BADGE1234"
         self.badgeclass = BadgeClassMock()
+        self.issued_on = None
+        self.expires_at = None
 
 class IssuerMock:
     def __init__(self):
         self.id = "ISS1234"
         self.name = "Mock Issuer"
 
-class TestCredentialsView(SimpleTestCase):
+class TestCredentialsSerializers(SimpleTestCase):
     def test_serializer_serializes_credential(self):
         badge_instance = BadgeInstanceMock()
         edu_credential = EduCredential("offer_id", "credential_configuration_id", badge_instance)
@@ -61,3 +65,30 @@ class TestCredentialsView(SimpleTestCase):
         actual_data = dict(serializer.data)
         self.maxDiff = None # Debug full diff
         self.assertDictEqual(actual_data, expected_data)
+
+    def test_optional_valid_from_field_set(self):
+        badge_instance = BadgeInstanceMock()
+        badge_instance.issued_on = DateTime.fromisoformat("2020-01-01:01:13:37")
+
+        edu_credential = EduCredential("offer_id", "credential_configuration_id", badge_instance)
+        actual_data = dict(EduCredentialSerializer(edu_credential).data)
+
+        self.assertEqual(actual_data["credential"]["validFrom"], "2020-01-01T01:13:37Z")
+
+    def test_optional_valid_from_field_notset(self):
+        badge_instance = BadgeInstanceMock()
+        badge_instance.issued_on = None
+
+        edu_credential = EduCredential("offer_id", "credential_configuration_id", badge_instance)
+        actual_data = dict(EduCredentialSerializer(edu_credential).data)
+
+        self.assertNotIn("validFrom", actual_data)
+
+    def test_optional_valid_until(self):
+        badge_instance = BadgeInstanceMock()
+        badge_instance.expires_at = DateTime.fromisoformat("2020-01-01:01:13:37")
+
+        edu_credential = EduCredential("offer_id", "credential_configuration_id", badge_instance)
+        actual_data = dict(EduCredentialSerializer(edu_credential).data)
+
+        self.assertEqual(actual_data["credential"]["validUntil"], "2020-01-01T01:13:37Z")
