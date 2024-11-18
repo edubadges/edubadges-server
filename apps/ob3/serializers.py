23c6c333d3ec49faf796b70761b1874b8d58ce69
Bèr Kessels <ber@berk.es>
2024-11-18 11:31:16 +0100

    feat: Add optional validFrom and validUntil fields to credential
    These are sourced from issued_on and expires_at fields on
    badge-instance.
    They won't be in the payload if the fields are Null (or blank or none)
    as per the OB3 spec.

--- apps/ob3/serializers.py
+++ apps/ob3/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from apps.mainsite.settings import UI_URL
@@ -52,7 +53,30 @@ class AchievementSubjectSerializer(serializers.Serializer):
 
 class CredentialSerializer(serializers.Serializer):
     issuer = IssuerSerializer()
+    validFrom = serializers.DateTimeField(
+            source='valid_from',
+            required=False,
+            allow_null=True,
+            default_timezone=timezone.utc
+    )
+    validUntil = serializers.DateTimeField(
+            source='valid_until',
+            required=False,
+            allow_null=True,
+            default_timezone=timezone.utc
+    )
     credentialSubject = AchievementSubjectSerializer(source='credential_subject')
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        """Remove a list of specific fields if they are None"""
+        to_remove = ['validFrom', 'validUntil']
+        for key in to_remove:
+            if ret.get(key) is None:
+                ret.pop(key)
+
+        return ret
 
 class EduCredentialSerializer(serializers.Serializer):
     offerId = serializers.CharField(source='offer_id')
