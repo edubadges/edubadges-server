c324f2a24b918e1cda44eb8b0afdedadecaa0871
Daniel <daniel.ostkamp@surf.nl>
2024-12-18 12:06:21 +0100

    For time being, only regular badge should be formal

--- apps/issuer/serializers.py
+++ apps/issuer/serializers.py
@@ -248,7 +248,7 @@ class BadgeClassSerializer(OriginalJsonSerializerMixin, ExtensionsSaverMixin,
         is_mbo = issuer.institution.institution_type == Institution.TYPE_MBO
         is_private = data.get("is_private", False)
         type_badge = data["badge_class_type"]
-        self.formal = type_badge in [BadgeClass.BADGE_CLASS_TYPE_REGULAR, BadgeClass.BADGE_CLASS_TYPE_MICRO]
+        self.formal = type_badge == BadgeClass.BADGE_CLASS_TYPE_REGULAR
         if is_mbo and not is_private:
             extensions += ["StudyLoadExtension"]
         if not is_private:
