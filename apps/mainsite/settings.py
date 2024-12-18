40249d2826a2ff4cc0aa8e12ced744883cfc317c
Daniel <daniel.ostkamp@surf.nl>
2024-12-18 10:53:13 +0100

    Updated default CACHE LOCATION to 0.0.0.0 to make it work on single node environments

--- apps/mainsite/settings.py
+++ apps/mainsite/settings.py
@@ -358,7 +358,7 @@ LOGGING = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': os.environ.get('MEMCACHED', 'localhost:11211'),
+        'LOCATION': os.environ.get('MEMCACHED', '0.0.0.0:11211'),
     }
 }
 
