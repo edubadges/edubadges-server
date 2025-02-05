1dce8429e85905e2d991f822ffd43da5c9eb9d49
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2025-02-05 16:56:27 +0100

    Updated seed data

--- apps/mainsite/seeds/02_users.py
+++ apps/mainsite/seeds/02_users.py
@@ -6,46 +6,51 @@ from allauth.socialaccount.models import SocialAccount
 
 from badgeuser.models import BadgeUser, TermsAgreement
 from institution.models import Institution
-from mainsite.seeds.constants import ENROLLED_STUDENT_EMAIL, REVOKED_STUDENT_EMAIL, INSTITUTION_UNIVERSITY_EXAMPLE_ORG, \
-    AWARDED_STUDENT_EMAIL
+from mainsite.seeds.constants import (
+    ENROLLED_STUDENT_EMAIL,
+    REVOKED_STUDENT_EMAIL,
+    INSTITUTION_UNIVERSITY_EXAMPLE_ORG,
+    AWARDED_STUDENT_EMAIL,
+)
 from staff.models import InstitutionStaff
 from .util import add_terms_institution
 
 # Institution
 institutions = [
-    {'name_english': INSTITUTION_UNIVERSITY_EXAMPLE_ORG,
-     'description_english': 'The university example is always a good place to hang out',
-     'description_dutch': 'De university example is altijd een goede plek om rond te hangen (wat een vertaling)',
-     'institution_type': 'WO'
-     },
-    {'name_english': 'diy.surfconext.nl',
-     'description_english': 'The university diy is also a good place to hang out',
-     'description_dutch': 'De university diy is ook een mooie plek',
-     'institution_type': 'HBO'
-     },
-    {'name_english': 'university1',
-     'description_english': 'University1 description',
-     'description_dutch': 'University1 beschrijving',
-     'institution_type': 'MBO'
-     },
-    {'name_english': 'university2',
-     'description_english': 'University1 description',
-     'description_dutch': 'University1 beschrijving',
-     'institution_type': 'HBO'
-     },
+    {
+        'name_english': INSTITUTION_UNIVERSITY_EXAMPLE_ORG,
+        'description_english': 'The university example is always a good place to hang out',
+        'description_dutch': 'De university example is altijd een goede plek om rond te hangen (wat een vertaling)',
+        'institution_type': 'WO',
+    },
+    {
+        'name_english': 'harvard-example.edu',
+        'description_english': 'Harward Example',
+        'description_dutch': 'Hardward Example',
+        'institution_type': 'MBO',
+    },
+    {
+        'name_english': 'yale-uni-example.edu',
+        'description_english': 'Yale Uni Example',
+        'description_dutch': 'Yale Uni Example',
+        'institution_type': 'HBO',
+    },
 ]
+
 for ins in institutions:
-    institution, _ = Institution.objects.get_or_create(identifier=ins['name_english'],
-                                                       name_english=ins['name_english'],
-                                                       description_english=ins['description_english'],
-                                                       description_dutch=ins['description_dutch'],
-                                                       institution_type=ins['institution_type'],
-                                                       image_english="uploads/institution/surf.png",
-                                                       image_dutch="uploads/institution/surf.png",
-                                                       grading_table="https://url.to.gradingtable/gradingtable.html",
-                                                       direct_awarding_enabled=True,
-                                                       micro_credentials_enabled=True,
-                                                       brin="000-7777-11111")
+    institution, _ = Institution.objects.get_or_create(
+        identifier=ins['name_english'],
+        name_english=ins['name_english'],
+        description_english=ins['description_english'],
+        description_dutch=ins['description_dutch'],
+        institution_type=ins['institution_type'],
+        image_english='uploads/institution/surf.png',
+        image_dutch='uploads/institution/surf.png',
+        grading_table='https://url.to.gradingtable/gradingtable.html',
+        direct_awarding_enabled=True,
+        micro_credentials_enabled=True,
+        brin='000-7777-11111',
+    )
     add_terms_institution(institution)
 
 
@@ -67,7 +72,7 @@ all_perms = {
     'may_delete': True,
     'may_award': True,
     'may_sign': True,
-    'may_administrate_users': True
+    'may_administrate_users': True,
 }
 
 no_perms = {
@@ -77,14 +82,20 @@ no_perms = {
     'may_delete': False,
     'may_award': False,
     'may_sign': False,
-    'may_administrate_users': False
+    'may_administrate_users': False,
 }
 
 
 def create_admin(username, email, first_name, last_name, institution_name, uid, perms=all_perms):
-    user, _ = BadgeUser.objects.get_or_create(username=username, email=email, last_name=last_name,
-                                              first_name=first_name, is_teacher=True, invited=True,
-                                              is_superuser=True)
+    user, _ = BadgeUser.objects.get_or_create(
+        username=username,
+        email=email,
+        last_name=last_name,
+        first_name=first_name,
+        is_teacher=True,
+        invited=True,
+        is_superuser=True,
+    )
 
     EmailAddress.objects.get_or_create(verified=1, primary=1, email=email, user=user)
     SocialAccount.objects.get_or_create(provider='surf_conext', uid=uid, user=user)
@@ -97,8 +108,9 @@ def create_admin(username, email, first_name, last_name, institution_name, uid, 
 
 
 def create_teacher(username, email, first_name, last_name, institution_name, uid, perms=no_perms):
-    user, _ = BadgeUser.objects.get_or_create(username=username, email=email, last_name=last_name,
-                                              first_name=first_name, is_teacher=True, invited=True)
+    user, _ = BadgeUser.objects.get_or_create(
+        username=username, email=email, last_name=last_name, first_name=first_name, is_teacher=True, invited=True
+    )
 
     EmailAddress.objects.get_or_create(verified=1, primary=1, email=email, user=user)
     SocialAccount.objects.get_or_create(provider='surf_conext', uid=uid, user=user)
@@ -112,120 +124,60 @@ def create_teacher(username, email, first_name, last_name, institution_name, uid
 institution_admins = [
     # staff1
     {
-        "username": "joseph+weeler",
-        "email": "Joseph+Weeler@university-example.org",
-        "first_name": "Joseph",
-        "last_name": "Wheeler",
-        "institution_name": "university-example.org",
-        "uid": "bf847baedbe7045394ea38de3c994f0332f2dfcb",
+        'username': 'joseph+weeler',
+        'email': 'Joseph+Weeler@university-example.org',
+        'first_name': 'Joseph',
+        'last_name': 'Wheeler',
+        'institution_name': 'university-example.org',
+        'uid': 'bf847baedbe7045394ea38de3c994f0332f2dfcb',
     },
-    # staff2
+    # professor 1
     {
-        "username": "anthony+west",
-        "email": "Anthony_West@university-example.org",
-        "first_name": "Anthony",
-        "last_name": "West",
-        "institution_name": "university-example.org",
-        "uid": "b57dc8a602a198b8f75f4a427e0464cd1029869a",
+        'username': 'p1u1',
+        'email': 'jordan@harvard-example.edu',
+        'first_name': 'Jordan',
+        'last_name': 'Belfort',
+        'institution_name': 'harvard-example.edu',
+        'uid': '86877d2f465c7ae597798bd2f929568904af023f',
     },
+    # teacher 3
     {
-        "username": "test12345",
-        "email": "thisisa@valid.email",
-        "first_name": "Firster",
-        "last_name": "LastNamer",
-        "institution_name": "university-example.org",
-        "uid": "5d0584e3bf178ab4f6d6c9e02ebbd195afd10853",
-    },
-    {
-        "username": "p1u1",
-        "email": "professor1@university1.org",
-        "first_name": "professor1",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "a20ccd72975606f20a05772182482b0eeb88dc09",
-    },
-    {
-        "username": "p2u1",
-        "email": "professor2@university1.org",
-        "first_name": "professor2",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "b40d3e0abe239e22c84605231de16a9ddea9698b",
-    },
-    {
-        "username": "p1u2",
-        "email": "professor1@university2.org",
-        "first_name": "professor1",
-        "last_name": "university2",
-        "institution_name": "university2",
-        "uid": "00020001",
+        'username': 'bbernanke',
+        'email': 'B.S.Bernanke@yale-uni-example.edu',
+        'first_name': 'Ben',
+        'last_name': 'Bernake',
+        'institution_name': 'yale-uni-example.edu',
+        'uid': '4f516a9d3c32c0a19a4e9fe05b3185feef43a5ae',
     },
 ]
 
 teachers = [
+    # professor 4
     {
-        "username": "test123456",
-        "email": "test123456@university-example.org",
-        "first_name": "Seconder",
-        "last_name": "LastNamer2",
-        "institution_name": "university-example.org",
-        "uid": "847292f4f83cfc3835d9f367c4ec659a4c844a0a",
+        'username': 'g_ohm',
+        'email': 'georg.ohm@university-example.org',
+        'first_name': 'Georg',
+        'last_name': 'Ohm',
+        'institution_name': 'university-example.org',
+        'uid': 'd2ba7c82a401a4c8f14b53d43af4d6c26712f971',
     },
+    # teacher 1
     {
-        "username": "p3u1",
-        "email": "professor3@university1.org",
-        "first_name": "professor3",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "e1d42cb2da86d8763053ea7c9e29ad87fcbc73aa",
+        'username': 'jstiglitz',
+        'email': 'Joseph.Stiglitz@harvard-example.edu',
+        'first_name': 'Joseph',
+        'last_name': 'Stiglitzller',
+        'institution_name': 'harvard-example.edu',
+        'uid': 'f981043d36d8fc3188275cc9fc3bad3ee492ea58',
     },
+    # teacher 4
     {
-        "username": "p4u1",
-        "email": "professor4@university1.org",
-        "first_name": "professor4",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "52622709ca4e5590a611d5f6f49f0712c7e513a8",
-    },
-    {
-        "username": "p5u1",
-        "email": "professor5@university1.org",
-        "first_name": "professor5",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "aaf4dda63707f0b2a59fdfaee75df0ce7579bbe0",
-    },
-    {
-        "username": "p6u1",
-        "email": "professor6@university1.org",
-        "first_name": "professor6",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "72ca9407474db806cfb457ac00a12dc601865591",
-    },
-    {
-        "username": "p7u1",
-        "email": "professor7@university1.org",
-        "first_name": "professor7",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "02ffce96ae4a9c318ae074054429a8fed7784442",
-    },
-    {
-        "username": "p8u1",
-        "email": "professor8@university1.org",
-        "first_name": "professor8",
-        "last_name": "university1",
-        "institution_name": "university1",
-        "uid": "9a988eab38ad87ee4398e0af6f5807d63255cf6a",
-    },
-    {
-        "username": "p2u2",
-        "email": "professor2@university2.org",
-        "first_name": "professor2",
-        "last_name": "university2",
-        "institution_name": "university2",
-        "uid": "00020002",
+        'username': 'agreenspan',
+        'email': 'A.Greenspan@yale-uni-example.edu',
+        'first_name': 'Alan',
+        'last_name': 'Greenspan',
+        'institution_name': 'yale-uni-example.edu',
+        'uid': '72c5c72c5fb00b0718366b45f9fbe14ececd3d6e',
     },
 ]
 
@@ -234,12 +186,17 @@ teachers = [
 [create_teacher(**t) for t in teachers]
 
 # Users - Students
-extra_data = json.dumps({"eduid": str(uuid.uuid4())})
+extra_data = json.dumps({'eduid': str(uuid.uuid4())})
 
 
 def create_student(username, first_name, last_name, email, uid):
-    user, _ = BadgeUser.objects.get_or_create(username=username, email=email, first_name=first_name,
-                                              last_name=last_name, validated_name=f"{first_name} {last_name}")
+    user, _ = BadgeUser.objects.get_or_create(
+        username=username,
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+        validated_name=f'{first_name} {last_name}',
+    )
     accept_terms(user)
 
     EmailAddress.objects.get_or_create(verified=1, primary=1, email=email, user=user)
@@ -250,33 +207,33 @@ def create_student(username, first_name, last_name, email, uid):
 
 students = [
     {
-        "username": "user",
-        "first_name": "John",
-        "last_name": "Doe",
-        "email": "edubadges.surf@gmail.com",
-        "uid": "5acb63239f298a0a7de0081cd4a603d807178846"
+        'username': 'user',
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'edubadges.surf@gmail.com',
+        'uid': '5acb63239f298a0a7de0081cd4a603d807178846',
     },
     {
-        "username": "mary",
-        "first_name": "Mary",
-        "last_name": "Doe",
-        "email": ENROLLED_STUDENT_EMAIL,
-        "uid": "7ec1acf9ce98835e29c337077491b4ba6d1ed21d"
+        'username': 'mary',
+        'first_name': 'Mary',
+        'last_name': 'Doe',
+        'email': ENROLLED_STUDENT_EMAIL,
+        'uid': '7ec1acf9ce98835e29c337077491b4ba6d1ed21d',
     },
     {
-        "username": "sarah",
-        "first_name": "Sarah",
-        "last_name": "Weirdćharacter",
-        "email": REVOKED_STUDENT_EMAIL,
-        "uid": "7fc994786c9e7815da17f5e97f796f67e891509e"
+        'username': 'sarah',
+        'first_name': 'Sarah',
+        'last_name': 'Weirdćharacter',
+        'email': REVOKED_STUDENT_EMAIL,
+        'uid': '7fc994786c9e7815da17f5e97f796f67e891509e',
     },
     {
-        "username": "carl",
-        "first_name": "Carl",
-        "last_name": "Doolittle",
-        "email": AWARDED_STUDENT_EMAIL,
-        "uid": "78b9ec1bb8731ec04b42137faf6a3c7068c89212"
-    }
+        'username': 'carl',
+        'first_name': 'Carl',
+        'last_name': 'Doolittle',
+        'email': AWARDED_STUDENT_EMAIL,
+        'uid': '78b9ec1bb8731ec04b42137faf6a3c7068c89212',
+    },
 ]
 
 [create_student(**s) for s in students]
--- apps/mainsite/seeds/05_staffmemberships.py
+++ apps/mainsite/seeds/05_staffmemberships.py
@@ -1,6 +1,191 @@
-from django.conf import settings
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
 
-from badgeuser.models import BadgeUser
-from issuer.models import BadgeClass
+from badgeuser.models import BadgeUser, TermsAgreement
+from institution.models import Faculty, Institution
+from issuer.models import BadgeClass, Issuer
 
-super_user = BadgeUser.objects.get(username=settings.SUPERUSER_NAME)
+
+def accept_terms(user):
+    if user.is_teacher:
+        terms = user.institution.cached_terms()
+        for term in terms:
+            terms_agreement, _ = TermsAgreement.objects.get_or_create(user=user, terms=term)
+            terms_agreement.agreed_version = term.version
+            terms_agreement.agreed = True
+            terms_agreement.save()
+
+
+all_perms = {
+    'may_create': True,
+    'may_read': True,
+    'may_update': True,
+    'may_delete': True,
+    'may_award': True,
+    'may_sign': True,
+    'may_administrate_users': True,
+}
+
+award_perms = {
+    'may_create': False,
+    'may_read': False,
+    'may_update': False,
+    'may_delete': False,
+    'may_award': True,
+    'may_sign': True,
+    'may_administrate_users': False,
+}
+
+
+def create_facultystaff(username, email, first_name, last_name, institution_name, faculty_name, uid, perms=all_perms):
+    user, _ = BadgeUser.objects.get_or_create(
+        username=username,
+        email=email,
+        last_name=last_name,
+        first_name=first_name,
+        is_teacher=True,
+        invited=True,
+        is_superuser=True,
+    )
+
+    EmailAddress.objects.get_or_create(verified=1, primary=1, email=email, user=user)
+    SocialAccount.objects.get_or_create(provider='surf_conext', uid=uid, user=user)
+
+    institution = Institution.objects.get(identifier=institution_name)
+    faculty = Faculty.objects.get(name_english=faculty_name, institution=institution)
+    user.institution = institution
+    accept_terms(user)
+    user.save()
+    faculty.create_staff_membership(user, perms)
+
+
+def create_issuerstaff(
+    username, email, first_name, last_name, institution_name, faculty_name, issuer_name, uid, perms=all_perms
+):
+    user, _ = BadgeUser.objects.get_or_create(
+        username=username, email=email, last_name=last_name, first_name=first_name, is_teacher=True, invited=True
+    )
+
+    EmailAddress.objects.get_or_create(verified=1, primary=1, email=email, user=user)
+    SocialAccount.objects.get_or_create(provider='surf_conext', uid=uid, user=user)
+
+    institution = Institution.objects.get(identifier=institution_name)
+    user.institution = institution
+    faculty = Faculty.objects.get(name_english=faculty_name, institution=institution)
+    issuer = Issuer.objects.get(name_english=issuer_name, faculty=faculty)
+    accept_terms(user)
+    user.save()
+    issuer.create_staff_membership(user, perms)
+
+
+def create_badgeclassstaff(
+    username,
+    email,
+    first_name,
+    last_name,
+    institution_name,
+    faculty_name,
+    issuer_name,
+    badgeclass_name,
+    uid,
+    perms=all_perms,
+):
+    user, _ = BadgeUser.objects.get_or_create(
+        username=username, email=email, last_name=last_name, first_name=first_name, is_teacher=True, invited=True
+    )
+
+    EmailAddress.objects.get_or_create(verified=1, primary=1, email=email, user=user)
+    SocialAccount.objects.get_or_create(provider='surf_conext', uid=uid, user=user)
+
+    institution = Institution.objects.get(identifier=institution_name)
+    user.institution = institution
+
+    faculty = Faculty.objects.get(name_english=faculty_name, institution=institution)
+    issuer = Issuer.objects.get(name_english=issuer_name, faculty=faculty)
+    badgeclass = BadgeClass.objects.get(name=badgeclass_name, issuer=issuer)
+    accept_terms(user)
+    user.save()
+    badgeclass.create_staff_membership(user, perms)
+
+
+issuer_group_staff = [
+    # staff2
+    {
+        'username': 'anthony',
+        'email': 'Anthony_West@university-example.org',
+        'first_name': 'Anthony',
+        'last_name': 'West',
+        'institution_name': 'university-example.org',
+        'faculty_name': 'Medicine',
+        'uid': '94d8812c425944e5b707ffd12bdf44f0ed71b809',
+    },
+    # staff3
+    {
+        'username': 'oburton',
+        'email': 'Osc@r__Burton@university-example.org',
+        'first_name': 'Oscar',
+        'last_name': 'Burton',
+        'institution_name': 'university-example.org',
+        'faculty_name': 'Medicine',
+        'uid': '802d1a5325b08dca8ce30fe7e92a69187abb8ead',
+        'perms': award_perms,
+    },
+]
+
+issuer_staff = [
+    # professor 5
+    {
+        'username': 'jrockefeller',
+        'email': 'John.D.Rockefeller@university-example.org',
+        'first_name': 'John Davison',
+        'last_name': 'Rockefeller',
+        'institution_name': 'university-example.org',
+        'faculty_name': 'Medicine',
+        'issuer_name': 'Medicine',
+        'uid': '981c9a944fe8ed05a0c33d40dc4da134ed6c0d93',
+    },
+    # teacher 2
+    {
+        'username': 'pkrugman',
+        'email': 'P.R.Krugman@harvard-example.edu',
+        'first_name': 'Paul',
+        'last_name': 'Krugman',
+        'institution_name': 'harvard-example.edu',
+        'faculty_name': 'Medicine',
+        'issuer_name': 'Medicine',
+        'uid': 'a400fdc5edda4ab3c99400506610c1bb062bedb8',
+        'perms': award_perms,
+    },
+]
+
+badgeclass_staff = [
+    # professor 2
+    {
+        'username': 'p2u1',
+        'email': 'S.Wynn@harvard-example.edu',
+        'first_name': 'Steve',
+        'last_name': 'Wynn',
+        'institution_name': 'harvard-example.edu',
+        'faculty_name': 'Medicine',
+        'issuer_name': 'Medicine',
+        'badgeclass_name': 'Growth and Development',
+        'uid': '19e584047361617f97a8b0cfa6a17555d68753c1',
+    },
+    # professor 3
+    {
+        'username': 'p1u2',
+        'email': 'isaacnewton@university-example.org',
+        'first_name': 'Isaac',
+        'last_name': 'Newton',
+        'institution_name': 'university-example.org',
+        'faculty_name': 'Medicine',
+        'issuer_name': 'Medicine',
+        'badgeclass_name': 'Growth and Development',
+        'uid': '838b97344478a4086f07cf66b29d93730580a023',
+        'perms': award_perms,
+    },
+]
+
+[create_facultystaff(**a) for a in issuer_group_staff]
+[create_issuerstaff(**a) for a in issuer_staff]
+[create_badgeclassstaff(**a) for a in badgeclass_staff]
