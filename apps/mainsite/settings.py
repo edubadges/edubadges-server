ce9cb702e01f8e65dd1e3e7dedb905d6895189c5
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2025-02-19 16:04:19 +0100

    Using loki log handler instead of syslog

--- apps/mainsite/settings.py
+++ apps/mainsite/settings.py
@@ -1,5 +1,4 @@
 import os
-from syslog import LOG_LOCAL6
 
 from mainsite import TOP_DIR
 from mainsite.environment import env_settings
@@ -308,15 +307,16 @@ debug_handlers = ['badgr_debug']
 
 if DOMAIN.startswith('acc') or DOMAIN.startswith('prod'):
     handlers = handlers | {
-        'badgr_debug_syslog': {
-            'level': 'DEBUG',
-            'formatter': 'badgr',
-            'class': 'logging.handlers.SysLogHandler',
-            'address': '("195.169.124.131", 514)',
-            'facility': LOG_LOCAL6,
+        'badgr_debug_loki': {
+            'level': 'DEBUG',  # Log level. Required
+            'class': 'django_loki.LokiHttpHandler',  # Required
+            'formatter': 'loki',  # Loki formatter. Required
+            'timeout': 0.5,  # Post request timeout, default is 0.5. Optional
+            'tags': {'badgr': 'debug'},  # Tags / Labels to attach to the log.
+            'url': 'http://slog.edubadges.nl:3100/loki/api/v1/push',  # Loki url.
         }
     }
-    debug_handlers.append('badgr_debug_syslog')
+    debug_handlers.append('badgr_debug_loki')
 
 LOGGING = {
     'version': 1,
@@ -346,6 +346,9 @@ LOGGING = {
             '()': 'mainsite.formatters.JsonFormatter',
             'format': '%(asctime)s',
             'datefmt': '%Y-%m-%dT%H:%M:%S%z',
+        },
+        'loki': {
+            'class': 'django_loki.LokiFormatter',  # required
         },
     },
     'filters': {
