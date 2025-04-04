fe7c9591088e258635c881635a2bd0c94463c16e
Daniel <daniel.ostkamp@surf.nl>
2025-04-04 08:08:08 +0200

    Updating docker setup for k8s

--- docker/entrypoint-k8s.sh
+++ docker/entrypoint-k8s.sh
@@ -3,6 +3,8 @@ set -e
 
 mkdir -p mediafiles/uploads/badges/
 cp uploads/badges/*.png mediafiles/uploads/badges/
+cp uploads/issuers/surf.png mediafiles/uploads/issuers/
+cp uploads/institution/surf.png mediafiles/uploads/institution/
 
 # Run migrations
 #echo "Running migrations"
