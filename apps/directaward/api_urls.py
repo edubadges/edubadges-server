ec3b2856e463f7e1150d9cb16186cde16e3bb5fa
Okke Harsta <oharsta@zilverline.com>
2025-04-29 12:29:47 +0200

    Removed resend endpoint

--- apps/directaward/api_urls.py
+++ apps/directaward/api_urls.py
@@ -5,7 +5,6 @@ from directaward.api import (
     DirectAwardBundleList,
     DirectAwardAccept,
     DirectAwardRevoke,
-    DirectAwardResend,
     DirectAwardDelete,
     DirectAwardBundleView,
 )
@@ -15,6 +14,5 @@ urlpatterns = [
     path('bundle/<str:entity_id>', DirectAwardBundleView.as_view(), name='direct_award_detail'),
     path('accept/<str:entity_id>', DirectAwardAccept.as_view(), name='direct_award_accept'),
     path('revoke-direct-awards', DirectAwardRevoke.as_view(), name='direct_award_revoke'),
-    path('resend-direct-awards', DirectAwardResend.as_view(), name='direct_award_resend'),
     path('delete-direct-awards', DirectAwardDelete.as_view(), name='direct_award_delete'),
 ]
