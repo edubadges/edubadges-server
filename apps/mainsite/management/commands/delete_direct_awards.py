557c341d5850e607f36b71fe3f667e3a842f4800
Okke Harsta <oharsta@zilverline.com>
2025-05-20 10:30:03 +0200

    Bugfix for F0X2cE1G

--- apps/mainsite/management/commands/delete_direct_awards.py
+++ apps/mainsite/management/commands/delete_direct_awards.py
@@ -1,14 +1,15 @@
-import logging
 import datetime
+import logging
 
 from django.core.management.base import BaseCommand
 from django.db import connections
-from django.utils.timezone import make_aware
+
 
 def _remove_cached_direct_awards(direct_award):
     direct_award.badgeclass.remove_cached_data(
         ['cached_direct_awards', 'cached_pending_direct_awards', 'cached_direct_award_bundles'])
     direct_award.bundle.remove_cached_data(['cached_direct_awards'])
+
 
 class Command(BaseCommand):
     """A command to delete direct awards with the status Deleted."""
@@ -23,7 +24,7 @@ class Command(BaseCommand):
         logger.info("Running delete_direct_awards")
 
         now = datetime.datetime.now(datetime.timezone.utc)
-        direct_awards = DirectAward.objects.filter(delete_at__lt=make_aware(now),
+        direct_awards = DirectAward.objects.filter(delete_at__lt=now,
                                                    status='Deleted').all()
 
         for direct_award in direct_awards:
