ce572d65fb092554d63c9f96ec4742d94bc51358
Bèr Kessels <ber@berk.es>
2025-08-07 10:13:26 +0200

    feat: re-implement Use preferred linked account for validated name
    Original here:
    
    ```
    commit 38f477ce1709f398d47755270ef5e95df92ca96b
    Author: Okke Harsta <oharsta@zilverline.com>
    Date:   Fri Jul 18 14:40:38 2025 +0200
    
        Use preferred linked account for validated name
    ````

--- apps/badgrsocialauth/tests/test_oidc_client.py
+++ apps/badgrsocialauth/tests/test_oidc_client.py
@@ -3,19 +3,20 @@ from badgrsocialauth.providers.eduid.oidc_client import EduIdUserInfo
 
 class EduIdUserInfoTest(SimpleTestCase):
 
-    def test_validated_names_in_info(self):
+    def test_validated_names_preferred(self):
         eppn_json = [
-            { 'validated_name': 'Hendrik-Jan Visser' },
+            { 'validated_name': 'Hendrik-Jan Visser', 'preferred': False },
+            { 'validated_name': 'Hennie Visser', 'preferred': True },
         ]
             
         subject = EduIdUserInfo(eppn_json)
         self.assertEqual(len(subject.validated_names()), 1)
-        self.assertIn('Hendrik-Jan Visser', subject.validated_names())
+        self.assertIn('Hennie Visser', subject.validated_names())
         
     def test_validated_names_multiple(self):
         eppn_json = [
-            { 'validated_name': 'Hendrik-Jan Visser' },
-            { 'validated_name': 'Hennie Visser' },
+            { 'validated_name': 'Hendrik-Jan Visser', 'preferred': True },
+            { 'validated_name': 'Hennie Visser', 'preferred': True },
         ]
             
         subject = EduIdUserInfo(eppn_json)
@@ -23,7 +24,8 @@ class EduIdUserInfoTest(SimpleTestCase):
         
     def test_validated_names_not_in_info(self):
         eppn_json = [
-            { 'name': 'Hendrik-Jan Visser' },
+            { 'name': 'Hendrik-Jan Visser' }, # no validated_name key
+            { 'validated_name': 'Hennie Visser' } # no preferred key
         ]
             
         subject = EduIdUserInfo(eppn_json)
