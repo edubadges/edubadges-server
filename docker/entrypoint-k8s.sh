00e7da087a1d1ecdd1ab24f4315d857ac66b0721
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2025-08-07 17:37:33 +0200

    Added mkdir for issuers and institution

--- docker/entrypoint-k8s.sh
+++ docker/entrypoint-k8s.sh
@@ -2,15 +2,10 @@
 set -e
 
 mkdir -p mediafiles/uploads/badges/
+mkdir -p mediafiles/uploads/issuers/
+mkdir -p mediafiles/uploads/institution/
 cp uploads/badges/*.png mediafiles/uploads/badges/
 cp uploads/issuers/surf.png mediafiles/uploads/issuers/
 cp uploads/institution/surf.png mediafiles/uploads/institution/
-
-# Run migrations
-#echo "Running migrations"
-#python ./manage.py migrate
-
-#echo "Seeding the database"
-#python ./manage.py seed
 
 exec "$@"
