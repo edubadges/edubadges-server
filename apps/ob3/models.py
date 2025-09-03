ae4c0568094357fe56932d0597a06ad4d3420837
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2025-09-03 15:49:23 +0200

    Added entity_id in SphereonOfferRequest when creating Credential

--- apps/ob3/models.py
+++ apps/ob3/models.py
@@ -3,7 +3,8 @@
 from hashlib import sha256
 from typing import Optional
 
-def generate_sha256_hashstring(identifier: str, salt: Optional[str]=None):
+
+def generate_sha256_hashstring(identifier: str, salt: Optional[str] = None):
     """
     Generate a SHA-256 hash string from an identifier and salt.
     This is now exactly the same as the one in issuer/utils, but that's accidental
@@ -16,8 +17,9 @@ def generate_sha256_hashstring(identifier: str, salt: Optional[str]=None):
     Returns:
         A SHA-256 hash string
     """
-    key = '{}{}'.format(identifier.lower(), salt if salt is not None else "")
+    key = '{}{}'.format(identifier.lower(), salt if salt is not None else '')
     return 'sha256$' + sha256(key.encode('utf-8')).hexdigest()
+
 
 # Mixin to provide dynamic field assignment to a class
 class StructFieldsMixin:
@@ -38,17 +40,18 @@ class StructFieldsMixin:
         # Check for invalid fields
         invalid_fields = set(kwargs.keys()) - set(self.FIELDS)
         if invalid_fields:
-            raise ValueError(f"Invalid fields provided: {invalid_fields}")
+            raise ValueError(f'Invalid fields provided: {invalid_fields}')
 
         # Check for missing fields
         missing_fields = set(self.FIELDS) - set(kwargs.keys())
         if missing_fields:
-            raise ValueError(f"Missing required fields: {missing_fields}")
+            raise ValueError(f'Missing required fields: {missing_fields}')
 
         for field in self.FIELDS:
             setattr(self, field, kwargs.get(field, None))
         for key, value in kwargs.items():
             setattr(self, key, value)
+
 
 # A plain old Python object (POPO) that represents an educational credential
 class ImpierceOfferRequest:
@@ -60,22 +63,25 @@ class ImpierceOfferRequest:
         self.credential = Credential(
             entity_id=badge_instance.entity_id,
             issuer=badge_instance.badgeclass.issuer,
-            valid_from = badge_instance.issued_on,
+            valid_from=badge_instance.issued_on,
             credential_subject=credential_subject,
         )
 
         if badge_instance.expires_at:
-            self.expires_at = badge_instance.expires_at.strftime("%Y-%m-%dT%H:%M:%SZ")
+            self.expires_at = badge_instance.expires_at.strftime('%Y-%m-%dT%H:%M:%SZ')
             self.credential.valid_until = badge_instance.expires_at
         else:
-            self.expires_at = "never"
+            self.expires_at = 'never'
+
 
 class SphereonOfferRequest:
-    def __init__(self, offer_id, credential_configuration_id, badge_instance, edu_id, email, eppn, family_name, given_name):
+    def __init__(
+        self, offer_id, credential_configuration_id, badge_instance, edu_id, email, eppn, family_name, given_name
+    ):
         self.credential_configuration_ids = [credential_configuration_id]
         self.grants = {
-            "authorization_code": {
-                "issuer_state": offer_id,
+            'authorization_code': {
+                'issuer_state': offer_id,
             }
         }
         credential_subject = AchievementSubject.from_badge_instance(badge_instance)
@@ -93,27 +99,6 @@ class SphereonOfferRequest:
         self.family_name = family_name
         self.given_name = given_name
 
-class SphereonOfferRequest:
-    def __init__(self, offer_id, credential_configuration_id, badge_instance, edu_id, email, eppn, family_name, given_name):
-        self.credential_configuration_ids = [credential_configuration_id]
-        self.grants = {
-            "authorization_code": {
-                "issuer_state": offer_id,
-            }
-        }
-        credential_subject = AchievementSubject.from_badge_instance(badge_instance)
-        self.credential = Credential(
-            issuer=badge_instance.badgeclass.issuer,
-            valid_from=badge_instance.issued_on,
-            credential_subject=credential_subject,
-        )
-        # TODO: Sphereon doesn't seem to support expiration dates yet, so we don't set it here.
-
-        self.edu_id = edu_id
-        self.email = email
-        self.eppn = eppn
-        self.family_name = family_name
-        self.given_name = given_name
 
 class Credential:
     def __init__(self, entity_id, issuer, valid_from, credential_subject, **kwargs):
@@ -124,9 +109,10 @@ class Credential:
 
         self.valid_until = kwargs.get('valid_until', None)
 
+
 class AchievementSubject:
     def __init__(self, achievement, identifier=None):
-        self.type = ["AchievementSubject"]
+        self.type = ['AchievementSubject']
         self.achievement = achievement
         self.identifier = identifier
 
@@ -139,10 +125,8 @@ class AchievementSubject:
         else:
             identifier = None
 
-        return AchievementSubject(
-                achievement=achievement,
-                identifier=identifier
-        )
+        return AchievementSubject(achievement=achievement, identifier=identifier)
+
 
 class IdentityObject:
     """
@@ -150,35 +134,31 @@ class IdentityObject:
     The name is a bit misleading, as it's not an object in the sense of a Python object,
     but contrary to all other "objects" in the OBv3 spec, it's what the spec calls it.
     """
+
     def __init__(self, recipient_identifier, salt, hasher=generate_sha256_hashstring):
-        self.identity_hash = hasher(
-                recipient_identifier,
-                salt
-        )
-        self.identity_type = "emailAddress"
+        self.identity_hash = hasher(recipient_identifier, salt)
+        self.identity_type = 'emailAddress'
         self.hashed = True
         self.salt = salt
 
     @staticmethod
     def from_badge_instance(badge_instance):
-        return IdentityObject(
-                badge_instance.recipient_identifier,
-                badge_instance.salt
-        )
+        return IdentityObject(badge_instance.recipient_identifier, badge_instance.salt)
+
 
 class Achievement(StructFieldsMixin):
     FIELDS = [
-            'id',
-            'criteria',
-            'description',
-            'ects',
-            'education_program_identifier',
-            'image',
-            'in_language',
-            'name',
-            'participation',
-            'alignment',
-            ]
+        'id',
+        'criteria',
+        'description',
+        'ects',
+        'education_program_identifier',
+        'image',
+        'in_language',
+        'name',
+        'participation',
+        'alignment',
+    ]
 
     @staticmethod
     def from_badge_instance(badge_instance):
@@ -187,25 +167,26 @@ class Achievement(StructFieldsMixin):
         ects = None
         education_program_identifier = None
 
-        if "extensions:LanguageExtension" in badge_class.extension_items:
-            in_language = badge_class.extension_items["extensions:LanguageExtension"]["Language"]
+        if 'extensions:LanguageExtension' in badge_class.extension_items:
+            in_language = badge_class.extension_items['extensions:LanguageExtension']['Language']
 
-        if "extensions:ECTSExtension" in badge_class.extension_items:
-            ects = badge_class.extension_items["extensions:ECTSExtension"]["ECTS"]
+        if 'extensions:ECTSExtension' in badge_class.extension_items:
+            ects = badge_class.extension_items['extensions:ECTSExtension']['ECTS']
 
-        if "extensions:EducationProgramIdentifierExtension" in badge_class.extension_items:
-            education_program_identifier = badge_class.extension_items["extensions:EducationProgramIdentifierExtension"]["EducationProgramIdentifier"]
+        if 'extensions:EducationProgramIdentifierExtension' in badge_class.extension_items:
+            education_program_identifier = badge_class.extension_items[
+                'extensions:EducationProgramIdentifierExtension'
+            ]['EducationProgramIdentifier']
 
         return Achievement(
             id=badge_instance.entity_id,
-            criteria= { "narrative": badge_class.criteria_text },
+            criteria={'narrative': badge_class.criteria_text},
             description=badge_class.description,
             name=badge_class.name,
-            image= { "id": badge_class.image_url() },
+            image={'id': badge_class.image_url()},
             in_language=in_language,
             ects=ects,
             education_program_identifier=education_program_identifier,
             participation=badge_class.participation,
-            alignment=badge_class.alignment_items
+            alignment=badge_class.alignment_items,
         )
-
