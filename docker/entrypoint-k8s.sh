c0791b065b62b96d77d00b5e0234b9103862494e
Daniel <daniel.ostkamp@surf.nl>
2025-09-13 13:14:09 +0200

    Add initial images via seed data to S3 object storage

--- docker/entrypoint-k8s.sh
+++ docker/entrypoint-k8s.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
-mkdir -p mediafiles/uploads/badges/
-mkdir -p mediafiles/uploads/issuers/
-mkdir -p mediafiles/uploads/institution/
-cp uploads/badges/*.png mediafiles/uploads/badges/
-cp uploads/issuers/surf.png mediafiles/uploads/issuers/
-cp uploads/institution/surf.png mediafiles/uploads/institution/
+# Only copy files to mediafiles if not using S3 storage
+if [ "${USE_S3,,}" != "true" ]; then
+    echo "Setting up local file storage..."
+    mkdir -p mediafiles/uploads/badges/
+    mkdir -p mediafiles/uploads/issuers/
+    mkdir -p mediafiles/uploads/institution/
+    cp uploads/badges/*.png mediafiles/uploads/badges/
+    cp uploads/issuers/surf.png mediafiles/uploads/issuers/
+    cp uploads/institution/surf.png mediafiles/uploads/institution/
+    echo "Local files copied to mediafiles/"
+else
+    echo "S3 storage is enabled. Initial images will be uploaded during seeding process."
+fi
 
 exec "$@"
