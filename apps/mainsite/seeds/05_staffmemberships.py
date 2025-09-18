ab99c73582e58c6249cf2ab125f3023a3ad63043
Daniel Ostkamp <4895210+Iso5786@users.noreply.github.com>
2025-09-18 09:36:43 +0200

    Updated seed data to have staff for EWI institutions

--- apps/mainsite/seeds/05_staffmemberships.py
+++ apps/mainsite/seeds/05_staffmemberships.py
@@ -121,20 +121,20 @@ issuer_group_staff = [
     # staff2
     {
         'username': 'anthony',
-        'email': 'Anthony_West@university-example.org',
+        'email': 'Anthony_West@mbob.nl',
         'first_name': 'Anthony',
         'last_name': 'West',
-        'institution_name': 'university-example.org',
+        'institution_name': 'mbob.nl',
         'faculty_name': 'Medicine',
         'uid': '94d8812c425944e5b707ffd12bdf44f0ed71b809',
     },
     # staff3
     {
         'username': 'oburton',
-        'email': 'Osc@r__Burton@university-example.org',
+        'email': 'Osc@r__Burton@uvh.nl',
         'first_name': 'Oscar',
         'last_name': 'Burton',
-        'institution_name': 'university-example.org',
+        'institution_name': 'uvh.nl',
         'faculty_name': 'Medicine',
         'uid': '802d1a5325b08dca8ce30fe7e92a69187abb8ead',
         'perms': award_perms,
@@ -142,20 +142,20 @@ issuer_group_staff = [
     # t eacher 7
     {
         'username': 'teacher7',
-        'email': 'teacher7@harvard-example.edu',
+        'email': 'teacher7@hbot.nl',
         'first_name': 'Teacher7',
         'last_name': 'East',
-        'institution_name': 'harvard-example.edu',
+        'institution_name': 'hbot.nl',
         'faculty_name': 'Medicine',
         'uid': 'c39bc64457075f9953c4608c2bd57f28bed6d2aa',
     },
     # teacher 8
     {
         'username': 'teacher8',
-        'email': 'teacher8@yale-uni-example.edu',
+        'email': 'teacher8@tun.nb',
         'first_name': 'Teacher8',
         'last_name': 'West',
-        'institution_name': 'yale-uni-example.edu',
+        'institution_name': 'tun.nb',
         'faculty_name': 'Medicine',
         'uid': '20c67628b85c421edfb2ca95c92654455cd69aa5',
     },
@@ -165,10 +165,10 @@ issuer_staff = [
     # professor 5
     {
         'username': 'jrockefeller',
-        'email': 'John.D.Rockefeller@university-example.org',
+        'email': 'John.D.Rockefeller@mbob.nl',
         'first_name': 'John Davison',
         'last_name': 'Rockefeller',
-        'institution_name': 'university-example.org',
+        'institution_name': 'mbob.nl',
         'faculty_name': 'Medicine',
         'issuer_name': 'Medicine',
         'uid': '981c9a944fe8ed05a0c33d40dc4da134ed6c0d93',
@@ -176,10 +176,10 @@ issuer_staff = [
     # teacher 2
     {
         'username': 'pkrugman',
-        'email': 'P.R.Krugman@harvard-example.edu',
+        'email': 'P.R.Krugman@uvh.nl',
         'first_name': 'Paul',
         'last_name': 'Krugman',
-        'institution_name': 'harvard-example.edu',
+        'institution_name': 'uvh.nl',
         'faculty_name': 'Medicine',
         'issuer_name': 'Medicine',
         'uid': 'a400fdc5edda4ab3c99400506610c1bb062bedb8',
@@ -188,10 +188,10 @@ issuer_staff = [
     # teacher 11
     {
         'username': 'teacher11',
-        'email': 'teacher11@harvard-example.edu',
+        'email': 'teacher11@hbot.nl',
         'first_name': 'Teacher11',
         'last_name': 'North',
-        'institution_name': 'harvard-example.edu',
+        'institution_name': 'hbot.nl',
         'faculty_name': 'Medicine',
         'issuer_name': 'Medicine',
         'uid': 'c3df59f7a8d884c5db8852aa5b60a813eb6a4183',
@@ -199,10 +199,10 @@ issuer_staff = [
     # teacher 12
     {
         'username': 'teacher12',
-        'email': 'teacher12@yale-uni-example.edu',
+        'email': 'teacher12@tun.nb',
         'first_name': 'Teacher12',
         'last_name': 'South',
-        'institution_name': 'yale-uni-example.edu',
+        'institution_name': 'tun.nb',
         'faculty_name': 'Medicine',
         'issuer_name': 'Medicine',
         'uid': '00b953fcc7849c084761b3a9163a475c9c700d88',
@@ -213,10 +213,10 @@ badgeclass_staff = [
     # professor 2
     {
         'username': 'p2u1',
-        'email': 'S.Wynn@harvard-example.edu',
+        'email': 'S.Wynn@uvh.nl',
         'first_name': 'Steve',
         'last_name': 'Wynn',
-        'institution_name': 'harvard-example.edu',
+        'institution_name': 'uvh.nl',
         'faculty_name': 'Medicine',
         'issuer_name': 'Medicine',
         'badgeclass_name': 'Growth and Development',
@@ -225,10 +225,10 @@ badgeclass_staff = [
     # professor 3
     {
         'username': 'p1u2',
-        'email': 'isaacnewton@university-example.org',
+        'email': 'isaacnewton@hbot.nl',
         'first_name': 'Isaac',
         'last_name': 'Newton',
-        'institution_name': 'university-example.org',
+        'institution_name': 'hbot.nl',
         'faculty_name': 'Medicine',
         'issuer_name': 'Medicine',
         'badgeclass_name': 'Growth and Development',
--- apps/mainsite/seeds/constants.py
+++ apps/mainsite/seeds/constants.py
@@ -1,15 +1,6 @@
-SURF_INSTITUTION_NAME = "SURF"
-EDU_BADGES_FACULTY_NAME = "eduBadges"
-EDU_BADGES_ISSUER_NAME = "Team edubadges"
-
-INSTITUTION_UNIVERSITY_EXAMPLE_ORG = "university-example.org"
-
-ENROLLED_STUDENT_EMAIL = "edubadges.surf+enrolled@gmail.com"
-REVOKED_STUDENT_EMAIL = "edubadges.surf+revoked@gmail.com"
-AWARDED_STUDENT_EMAIL = "edubadges.surf+awarded@gmail.com"
-DEMO_STUDENT_EMAIL = "student19example@gmail.com"
-DEMO_STUDENT_EPPN = "student19@university-example.org"
-# Password1!
+SURF_INSTITUTION_NAME = 'SURF'
+EDU_BADGES_FACULTY_NAME = 'eduBadges'
+EDU_BADGES_ISSUER_NAME = 'Team edubadges'
 
 BADGE_CLASS_INTRODUCTION_TO_PSYCHOLOGY = 'Introduction to Psychology'
 BADGE_CLASS_COGNITIVE_PSYCHOLOGY = 'Cognitive Psychology'
