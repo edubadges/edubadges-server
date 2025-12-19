b6f6ee30236cf33f54877aacac16d29042fcfe16
Bèr Kessels <ber@berk.es>
2025-12-19 11:28:48 +0100

    feat: Add all data provided by courses.csv to seeded badgeclasses
    This adds attributes such as language, assessment types, time investment
    etc.
    
    Instead of a fixed list of extensions, we build them dynamically based
    on the data in the seed csv.
    And we set a few attributes on the badgeclass from data in the CSV.

--- apps/mainsite/seeds/04_badgeclasses.py
+++ apps/mainsite/seeds/04_badgeclasses.py
@@ -1,42 +1,86 @@
 import json
+from typing import Any
 from venv import logger
-from django.conf import settings
 
-from institution.models import Institution, Faculty
 from badgeuser.models import BadgeUser
-from issuer.models import Issuer, BadgeClass, BadgeClassExtension
-
+from django.conf import settings
+from institution.models import Faculty, Institution
+from issuer.models import BadgeClass, BadgeClassExtension, Issuer
 from mainsite.seeds.util import institution_id_by_pba_id, read_seed_csv, seed_image_for
 
 super_user = BadgeUser.objects.get(username=settings.SUPERUSER_NAME)
 
-badge_class_extensions = {
-    'extensions:LanguageExtension': {
-        '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/LanguageExtension/context.json',
-        'type': ['Extension', 'extensions:LanguageExtension'],
-        'Language': 'nl_NL',
-    },
-    'extensions:ECTSExtension': {
-        '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/ECTSExtension/context.json',
-        'type': ['Extension', 'extensions:ECTSExtension'],
-        'ECTS': 2.5,
-    },
-    'extensions:EQFExtension': {
-        '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/EQFExtension/context.json',
-        'type': ['Extension', 'extensions:EQFExtension'],
-        'EQF': 7,
-    },
-    'extensions:LearningOutcomeExtension': {
-        '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/LearningOutcomeExtension/context.json',
-        'type': ['Extension', 'extensions:LearningOutcomeExtension'],
-        'LearningOutcome': 'Will appreciate the benefits of learning a foreign language.',
-    },
-    'extensions:EducationProgramIdentifierExtension': {
-        '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/EducationProgramIdentifierExtension/context.json',
-        'type': ['Extension', 'extensions:EducationProgramIdentifierExtension'],
-        'EducationProgramIdentifier': [56823],
-    },
-}
+
+def map_language(language: str) -> str:
+    return {'nl': 'nl_NL', 'en': 'en_EN'}.get(language, 'en_EN')
+
+
+def map_yes_no(value: str) -> bool:
+    return value.lower() == 'yes'
+
+
+def badge_class_extensions(
+    learning_outcome: str,
+    ects: float,
+    studyload: int,
+    time_investment: int,
+    eqf: int,
+    language: str,
+    education_program_identifier: int,
+) -> dict[str, dict[str, Any]]:
+    # We must add one extension based on which attribute has a value
+    if ects:
+        load_extension = {
+            'extensions:ECTSExtension': {
+                '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/ECTSExtension/context.json',
+                'type': ['Extension', 'extensions:ECTSExtension'],
+                'ECTS': ects,
+            }
+        }
+    elif studyload:
+        load_extension = {
+            'extensions:StudyLoadExtension': {
+                '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/StudyLoadExtension/context.json',
+                'type': ['Extension', 'extensions:StudyLoadExtension'],
+                'StudyLoad': studyload,
+            }
+        }
+    elif time_investment:
+        load_extension = {
+            'extensions:TimeInvestmentExtension': {
+                '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/TimeInvestmentExtension/context.json',
+                'type': ['Extension', 'extensions:TimeInvestmentExtension'],
+                'TimeInvestment': time_investment,
+            }
+        }
+    else:
+        load_extension = {}
+
+    extensions = {
+        'extensions:LanguageExtension': {
+            '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/LanguageExtension/context.json',
+            'type': ['Extension', 'extensions:LanguageExtension'],
+            'Language': language,
+        },
+        'extensions:EQFExtension': {
+            '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/EQFExtension/context.json',
+            'type': ['Extension', 'extensions:EQFExtension'],
+            'EQF': eqf,
+        },
+        'extensions:LearningOutcomeExtension': {
+            '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/LearningOutcomeExtension/context.json',
+            'type': ['Extension', 'extensions:LearningOutcomeExtension'],
+            'LearningOutcome': learning_outcome,
+        },
+        'extensions:EducationProgramIdentifierExtension': {
+            '@context': f'{settings.EXTENSIONS_ROOT_URL}/extensions/EducationProgramIdentifierExtension/context.json',
+            'type': ['Extension', 'extensions:EducationProgramIdentifierExtension'],
+            'EducationProgramIdentifier': [education_program_identifier],
+        },
+    }
+
+    return {**extensions, **load_extension}
+
 
 for course in read_seed_csv('courses'):
     institution_id = institution_id_by_pba_id(course['CourseID'].split('.')[0])
@@ -72,8 +116,24 @@ for course in read_seed_csv('courses'):
             'criteria_text': course['Criteria'],
             'created_by': super_user,
             'updated_by': super_user,
+            'assessment_type': course['Type of Assesment'],  #  NOTE the typo is intentional
+            'participation': course['Form of participation'],
+            'assessment_supervised': map_yes_no(course['Supervised']),
+            'assessment_id_verified': map_yes_no(course['Verified identity']),
+            'quality_assurance_name': course['Quality Assurance Name'],
+            'quality_assurance_description': course['Quality Assurance Description'],
+            'quality_assurance_url': course['Quality Assurance URL'],
         },
     )
 
-    for key, value in badge_class_extensions.items():
+    extensions = badge_class_extensions(
+        learning_outcome=course['LearningOutcome'],
+        ects=course['ECTS'],
+        studyload=course['Studyload'],
+        time_investment=course['TimeInvestment'],
+        eqf=course['EQF'],
+        language=map_language(course['Language']),
+        education_program_identifier=course['ProgrammeID'],
+    )
+    for key, value in extensions.items():
         BadgeClassExtension.objects.get_or_create(badgeclass=badgeclass, name=key, original_json=json.dumps(value))
