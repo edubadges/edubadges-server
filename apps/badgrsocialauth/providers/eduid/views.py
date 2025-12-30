143abcd5b82f6c1d6a6ca7b6647deece8e038efe
Bèr Kessels <ber@berk.es>
2025-12-30 09:35:30 +0100

    Revert "doc: Clarify who will consume and decode the base64 encoded next path"
    This reverts commit c6d096d45e16a9e47eee850a519a04968d2715d3.

--- apps/badgrsocialauth/providers/eduid/views.py
+++ apps/badgrsocialauth/providers/eduid/views.py
@@ -143,8 +143,7 @@ def after_terms_agreement(request, **kwargs):
     """
     this is the second part of the callback, after consent has been given, or is user already exists
     """
-    # Next is base64 encoded and kept that way here to avoid the redirect interpreting the value as part of the URL.
-    # The frontend is responsible for base64 decoding this value before using it for navigation/redirection.
+    # Next is base64 encoded, we keep it that way to avoid the redirect interpreting the value as part of the URL.
     badgr_app_pk, next = json.loads(kwargs['state'])
     try:
         badgr_app_pk = int(badgr_app_pk)
