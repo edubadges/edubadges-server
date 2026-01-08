4a405354b0fdce6739336c88fdd0f7c82da83fc5
Thomas Kalverda <thomas.kalverda@surf.nl>
2026-01-08 13:09:12 +0100

    Add dedicated settings for testing

--- apps/mainsite/settings_tests.py
+++ apps/mainsite/settings_tests.py
@@ -5,20 +5,3 @@ from .settings import *
 
 # disable logging for tests
 LOGGING = {}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'badgr_server',
-        'OPTIONS': {
-            "init_command": "SET default_storage_engine=InnoDB",
-        },
-    }
-}
-
-CELERY_ALWAYS_EAGER = True
-SECRET_KEY = 'aninsecurekeyusedfortesting'
-UNSUBSCRIBE_SECRET_KEY = str(SECRET_KEY)
-PAGINATION_SECRET_KEY = Fernet.generate_key()
-AUTHCODE_SECRET_KEY = Fernet.generate_key()
-DEFAULT_DOMAIN = 'https://badgr-pilot2.edubadges.nl'
