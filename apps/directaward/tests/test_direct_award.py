72c1e0e822ef9fb619f28d2c3154e82e51666b35
Thomas Kalverda <thomas.kalverda@surf.nl>
2026-01-12 09:56:27 +0100

    Remove edit directaward functionality from tests
    Functionality itself was removed in 72d6783

--- apps/directaward/tests/test_direct_award.py
+++ apps/directaward/tests/test_direct_award.py
@@ -95,19 +95,13 @@ class DirectAwardTest(BadgrTestCase):
                                     content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
-    def test_update_and_revoke_direct_award(self):
+    def test_revoke_direct_award(self):
         teacher1 = self.setup_teacher(authenticate=True)
         self.setup_staff_membership(teacher1, teacher1.institution, may_award=True)
         faculty = self.setup_faculty(institution=teacher1.institution)
         issuer = self.setup_issuer(created_by=teacher1, faculty=faculty)
         badgeclass = self.setup_badgeclass(issuer=issuer)
         direct_award = self.setup_direct_award(created_by=teacher1, badgeclass=badgeclass)
-        post_data = {'recipient_email': 'other@email.com'}
-        response = self.client.put('/directaward/edit/{}'.format(direct_award.entity_id), json.dumps(post_data),
-                                    content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(direct_award.__class__.objects.get(pk=direct_award.pk).recipient_email,
-                         'other@email.com')
         response = self.client.post('/directaward/revoke-direct-awards',
                                       json.dumps({'revocation_reason': 'revocation_reason',
                                                   'direct_awards': [{'entity_id': direct_award.entity_id}]}),
