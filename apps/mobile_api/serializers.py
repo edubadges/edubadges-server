2bf5c5983c5b3d21cd15ae190ab7811616d99c75
Thomas Kalverda <thomas.kalverda@surf.nl>
2026-01-26 16:40:13 +0100

    Add registration and consent data to user profile

--- apps/mobile_api/serializers.py
+++ apps/mobile_api/serializers.py
@@ -248,7 +248,7 @@ class TermsAgreementSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     termsagreement_set = TermsAgreementSerializer(many=True, read_only=True)
-    terms_agreed = serializers.SerializerMethodField()
+    terms_agreed = serializers.BooleanField(source='terms_agreed', read_only=True)
 
     class Meta:
         model = BadgeUser
@@ -263,12 +263,11 @@ class UserSerializer(serializers.ModelSerializer):
             'termsagreement_set',
         ]
 
-    def get_terms_agreed(self, obj):
-        return obj.general_terms_accepted()
-
 
 class UserProfileSerializer(serializers.ModelSerializer):
     institution = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    termsagreement_set = TermsAgreementSerializer(many=True, read_only=True)
+    terms_agreed = serializers.BooleanField(source='terms_agreed', read_only=True)
 
     class Meta:
         model = BadgeUser
@@ -280,6 +279,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'institution',
             'marketing_opt_in',
             'is_superuser',
+            'validated_name',
+            'schac_homes',
+            'terms_agreed',
+            'termsagreement_set',
         ]
 
 
