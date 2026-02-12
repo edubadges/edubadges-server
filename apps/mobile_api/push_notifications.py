8d3833870c53893af7319e0f506368681dc94801
Thomas Kalverda <thomas.kalverda@surf.nl>
2026-02-12 12:51:14 +0100

    Make push notification sending fail gracefully
    For when the service account json file is missing

--- apps/mobile_api/push_notifications.py
+++ apps/mobile_api/push_notifications.py
@@ -2,6 +2,7 @@ import logging
 
 from fcm_django.models import FCMDevice
 from firebase_admin import messaging
+from google.auth.exceptions import DefaultCredentialsError
 
 
 logger = logging.getLogger(__name__)
@@ -18,15 +19,22 @@ def send_push_notification(user, title, body, data):
         )
 
     logger.info(f"Sending push to {devices.count()} devices for user {user.id} ({user.entity_id})")
-    firebase_response = devices.send_message(message=message)
-
-    batch_response = firebase_response.response
-    if batch_response.failure_count > 0:
-        logger.warning(
-            f"{batch_response.failure_count} push notifications failed. "
-            f"Deactivated devices: {firebase_response.deactivated_registration_ids}"
-        )
+    try:
+        firebase_response = devices.send_message(message=message)
+    except DefaultCredentialsError as e:
+        logger.error(f"Cannot send FCM push: credentials file missing or unreadable. {e}")
+        return None
+    except Exception as e:
+        logger.error(f"Failed to send push: {e}")
+        return None
     else:
-        logger.info(f"All {batch_response.success_count} push notifications sent successfully")
+        batch_response = firebase_response.response
+        if batch_response.failure_count > 0:
+            logger.warning(
+                f"{batch_response.failure_count} push notifications failed. "
+                f"Deactivated devices: {firebase_response.deactivated_registration_ids}"
+            )
+        else:
+            logger.info(f"All {batch_response.success_count} push notifications sent successfully")
 
-    return firebase_response
+        return firebase_response
