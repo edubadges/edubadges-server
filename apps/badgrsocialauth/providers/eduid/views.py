10185ddf8625f2866881dddf1c0cf2ab0f8c7dd2
Daniel <d.ostkamp@posteo.de>
2026-06-25 16:43:28 +0200

    Fix: re-added eduperson_principal_name to claims towards SURFconext

--- apps/badgrsocialauth/providers/eduid/views.py
+++ apps/badgrsocialauth/providers/eduid/views.py
@@ -42,10 +42,10 @@ def login(request: HttpRequest):
         'state': state,
         'client_id': settings.EDU_ID_CLIENT,
         'response_type': 'code',
-        'scope': 'openid eduid.nl/links',
+        'scope': 'openid eduid.nl/links profile',
         'redirect_uri': f'{settings.HTTP_ORIGIN}/account/eduid/login/callback/',
         'claims': '{"id_token":{"preferred_username":null,"given_name":null,"family_name":null,"email":null,'
-        '"eduid":null, "eduperson_scoped_affiliation":null, "preferred_username":null, "uids":null}}',
+        '"eduid":null, "eduperson_scoped_affiliation":null, "eduperson_principal_name":null, "preferred_username":null, "uids":null}}',
     }
     validate_name = request.GET.get('validateName')
     if validate_name and validate_name.lower() == 'true':
